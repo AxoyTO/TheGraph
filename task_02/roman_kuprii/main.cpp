9c47e73fd3aa21d289bac2bc2dec5b981f4f2a34
roman <roman.kupry@gmail.com>
2021-09-25 14:13:42 +0300

    changed the logic of the write and convert functions

--- task_02/roman_kuprii/main.cpp
+++ task_02/roman_kuprii/main.cpp
@@ -6,18 +6,11 @@
 using std::cout;
 using std::endl;
 using std::vector;
+using std::to_string;
 
 //using namespace std;
 
-struct Vertex {
-    int id;
-    int edges;
-
-    Vertex(int _id = -1, int edg = 0): id(_id), edges(edg) {}
-};
-
 struct Edge {
-//TODO check uniqueness of ID field
     int id;
     std::array<int, 2> nodes;
 
@@ -25,13 +18,44 @@ struct Edge {
         nodes[0] = start;
         nodes[1] = end;
     }
+
+    std::string to_json() const {
+        std::string res;
+        res = "{ \"id\": ";
+        res += to_string(id);
+        res += ", \"vertex_ids\": [";
+        res += to_string(nodes[0]);
+        res += ", ";
+        res += to_string(nodes[1]);
+        res += "] }";
+        return res;
+    }
+
+};
+
+//TODO check uniqueness of ID field
+struct Vertex {
+    int id;
+    std::vector<int> edges_ids;
+
+    Vertex(int _id = -1): id(_id) {}
+
+    std::string to_json() const {
+        std::string res;
+        res = "{ \"id\": ";
+        res += to_string(id) + ", \"edge_ids\": [";
+        for (int n : edges_ids) {
+            res += to_string(n);
+            res += ", ";
+        }
+        res.pop_back();
+        res.pop_back();
+        res += "] }";
+        return res;
+    }
 };
 
 class Graph {
-    int size_;
-    vector<Vertex> vertices_;
-    vector<Edge> edges_;
-
 public:
     Graph(): size_(0) {}
 
@@ -41,56 +65,74 @@ public:
         size_ = vertices_.size();
     }
 
-//TODO: remove this function from Graph's ownership
-    int write_json() {
-        std::ofstream out;
-        out.open("test_graph.json", std::ofstream::out | std::ofstream::trunc);
-        out << "{\n\t\"vertices\": [\n\t\t";
-        for (int i = 0; i < size_; i++) {
-            out << "{\n\t\t\t\"id\": " << i << ",\n\t\t\t\"edge_ids\": [";
-            int cnt(0);
-            for (auto it = edges_.begin(); it != edges_.end(); it++) {
-                if ((it->nodes[0] == i) || (it->nodes[1] == i)) {
-                    cnt++;
-                    if (cnt < vertices_[i].edges) {
-                        out << it->id << ", ";
-                    } else out << (*it).id;
-                }
-            }
-            out << "]\n\t\t}";
-            if (i != (size_-1)) out << ", ";
+    std::string to_json() const {
+        std::string res;
+        res = "{ \"vertices\": [ ";
+        for (const auto & v_it : vertices_) {
+            res += v_it.to_json();
+            res += ", ";
         }
-        out << "\n\t],\n\t\"edges\": [\n\t\t";
-        for(auto it = edges_.begin(); it != edges_.end(); it++) {
-            out << "{\n\t\t\t\"id\": " << it->id << ",\n\t\t\t\"vertex_ids\": [" << it->nodes[0] << ", " << it->nodes[1] << "]\n\t\t}";
-            if (it != edges_.end()-1) {
-                out << ", ";
-            }
+        res.pop_back();
+        res.pop_back();
+        res += " ], \"edges\": [ ";
+        for (const auto & e_it : edges_) {
+            res += e_it.to_json();
+            res += ", ";
         }
-        out << "\n\t]\n}\n";
-        out.close();
-        return 0;
+        res.pop_back();
+        res.pop_back();
+        res += " ] }\n";
+        return res;
     }
 
+private:
+    int size_;
+    vector<Vertex> vertices_;
+    vector<Edge> edges_;
 };
 
+int write_graph(const std::string json_name, Graph A) {
+    std::ofstream out;
+    out.open(json_name, std::ofstream::out | std::ofstream::trunc);
+
+    out << A.to_json();
+
+    out.close();
+    return 0;
+}
+
 int main() {
+    // edge structure: {first vertex, second vertex, edge id}
     vector<Edge> init_edges = { {0,1,0}, {0,2,1}, {0,3,2}, {1,4,3}, {1,5,4}, {1,6,5}, {2,7,6}, {2,8,7},
                             {3,9,8}, {4,10,9}, {5,10,10}, {6,10,11}, {7,11,12}, {8,11,13}, {9,12,14},
                             {10,13,15}, {11,13,16}, {12,13,17} };
 
-    vector<Vertex> init_vertices = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    vector<Vertex> init_vertices;
 
-//    for (auto it = init_edges.begin(); it != init_edges.end(); it++) {
-//        init_vertices[(*it).nodes[0]].edges++;
-//        init_vertices[(*it).nodes[1]].edges++;
     for (const auto &it : init_edges) {
-        init_vertices[it.nodes[0]].edges++;
-        init_vertices[it.nodes[1]].edges++;
+
+//1. check if verex id is uniq
+        bool if_uniq0 = true, if_uniq1 = true;
+        for (const auto &v_it : init_vertices) {
+            if_uniq0 *= it.nodes[0] != v_it.id;
+            if_uniq1 *= it.nodes[1] != v_it.id;
+        }
+
+//2. add new vertex, if needed
+        if (if_uniq0) {
+            init_vertices.push_back(it.nodes[0]);
+        }
+        if (if_uniq1) {
+            init_vertices.push_back(it.nodes[1]);
+        }
+
+//3. add info about edge id into connected vetices
+        init_vertices[it.nodes[0]].edges_ids.push_back(it.id);
+        init_vertices[it.nodes[1]].edges_ids.push_back(it.id);
     }
 
     Graph A(init_edges, init_vertices);
-    A.write_json();
+    write_graph("test.json", A);
 
     return 0;
 }
