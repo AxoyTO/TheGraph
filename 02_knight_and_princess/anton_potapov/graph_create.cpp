a12b74e117d01db7df7f56c5dd29d9944568b7f8
Flagro <antonpgrey@gmail.com>
2021-10-06 23:02:38 +0300

    constructor is now meaningful

--- 02_knight_and_princess/anton_potapov/graph_create.cpp
+++ 02_knight_and_princess/anton_potapov/graph_create.cpp
@@ -1,10 +1,8 @@
-#include <algorithm>
 #include <cassert>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
-#include <map>
-#include <set>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -16,7 +14,8 @@ using EdgeId = int;
 struct Vertex {
   VertexId id;
   Camp camp_info;
-  Vertex(int vertex_id, Camp c_info = Camp()) : id(vertex_id), camp_info(c_info) {}
+  Vertex(int vertex_id, Camp c_info = Camp())
+      : id(vertex_id), camp_info(c_info) {}
 };
 
 struct Edge {
@@ -29,46 +28,17 @@ struct Edge {
 
 class Graph {
 public:
-  Graph(const std::string &s) {
-    size_t n = 0;
-    std::vector<Vertex> v_ans;
-    std::vector<Edge> e_ans;
-    std::vector<std::vector<VertexId>> a_ans;
-    if (s == "task_02") {
-      n = 14;
-      a_ans = std::vector<std::vector<VertexId>>(n, std::vector<VertexId>());
-      for (size_t i = 0; i < n; ++i) {
-        v_ans.push_back(Vertex(i));
-      }
-      e_ans.push_back(Edge(0, 0, 1));
-      e_ans.push_back(Edge(1, 0, 2));
-      e_ans.push_back(Edge(2, 0, 3));
-      e_ans.push_back(Edge(3, 1, 4));
-      e_ans.push_back(Edge(4, 1, 5));
-      e_ans.push_back(Edge(5, 1, 6));
-      e_ans.push_back(Edge(6, 2, 7));
-      e_ans.push_back(Edge(7, 2, 8));
-      e_ans.push_back(Edge(8, 3, 9));
-      e_ans.push_back(Edge(9, 4, 10));
-      e_ans.push_back(Edge(10, 5, 10));
-      e_ans.push_back(Edge(11, 6, 10));
-      e_ans.push_back(Edge(12, 7, 11));
-      e_ans.push_back(Edge(13, 8, 11));
-      e_ans.push_back(Edge(14, 9, 12));
-      e_ans.push_back(Edge(15, 10, 13));
-      e_ans.push_back(Edge(16, 11, 13));
-      e_ans.push_back(Edge(17, 12, 13));
-      for (const auto &el : e_ans) {
-        a_ans[el.vertex1_id].push_back(el.id);
-        a_ans[el.vertex2_id].push_back(el.id);
-      }
+  Graph() = default;
+  Graph(const std::vector<Vertex> &vertices, const std::vector<Edge> &edges)
+      : vertices_(vertices), edges_(edges) {
+    adjacency_list_ = std::vector<std::vector<VertexId>>(
+        vertices.size(), std::vector<VertexId>());
+    for (const auto &edge : edges) {
+      adjacency_list_[edge.vertex1_id].push_back(edge.id);
+      adjacency_list_[edge.vertex2_id].push_back(edge.id);
     }
-    vertices_ = v_ans;
-    edges_ = e_ans;
-    adjacency_list_ = a_ans;
   }
-
-  std::string get_json_string() {
+  std::string get_json_string() const {
     std::stringstream json_stringstream;
     json_stringstream << "{\n";
 
@@ -79,13 +49,13 @@ public:
       json_stringstream << "\t\t\t\"edge_ids\": [";
       for (size_t j = 0; j < adjacency_list_[i].size(); ++j) {
         json_stringstream << adjacency_list_[i][j];
-        if (j != adjacency_list_[i].size() - 1) {
+        if (j + 1 != adjacency_list_[i].size()) {
           json_stringstream << ", ";
         }
       }
       json_stringstream << "]\n";
       json_stringstream << "\t\t}";
-      if (i == adjacency_list_.size() - 1) {
+      if (i + 1 == adjacency_list_.size()) {
         json_stringstream << "\n";
       } else {
         json_stringstream << ",\n";
@@ -97,10 +67,10 @@ public:
     for (size_t i = 0; i < edges_.size(); ++i) {
       json_stringstream << "\t\t{\n";
       json_stringstream << "\t\t\t\"id\": " << edges_[i].id << ",\n";
-      json_stringstream << "\t\t\t\"vertex_ids\": [" << edges_[i].vertex1_id << ", "
-                << edges_[i].vertex2_id << "]\n";
+      json_stringstream << "\t\t\t\"vertex_ids\": [" << edges_[i].vertex1_id
+                        << ", " << edges_[i].vertex2_id << "]\n";
       json_stringstream << "\t\t}";
-      if (i == edges_.size() - 1) {
+      if (i + 1 == edges_.size()) {
         json_stringstream << "\n";
       } else {
         json_stringstream << ",\n";
@@ -118,10 +88,25 @@ private:
   std::vector<std::vector<VertexId>> adjacency_list_;
 };
 
+Graph task_02_get_graph(void) {
+  size_t n = 14;
+  std::vector<Vertex> v_ans;
+  for (size_t i = 0; i < n; ++i) {
+    v_ans.push_back(Vertex(i));
+  }
+  std::vector<Edge> e_ans{Edge(0, 0, 1),    Edge(1, 0, 2),    Edge(2, 0, 3),
+                          Edge(3, 1, 4),    Edge(4, 1, 5),    Edge(5, 1, 6),
+                          Edge(6, 2, 7),    Edge(7, 2, 8),    Edge(8, 3, 9),
+                          Edge(9, 4, 10),   Edge(10, 5, 10),  Edge(11, 6, 10),
+                          Edge(12, 7, 11),  Edge(13, 8, 11),  Edge(14, 9, 12),
+                          Edge(15, 10, 13), Edge(16, 11, 13), Edge(17, 12, 13)};
+  return Graph(v_ans, e_ans);
+}
+
 int main() {
-  Graph g("task_02");
+  Graph g = task_02_get_graph();
   std::string json_string = g.get_json_string();
-  
+
   std::fstream json_file;
   json_file.open("graph.json", std::ios::out);
   assert(json_file && "file not created");
