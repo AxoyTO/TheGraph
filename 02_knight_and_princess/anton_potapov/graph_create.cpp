bdc50c231c9847b00be4c3839c394b0f8de2dedc
Flagro <antonpgrey@gmail.com>
2021-10-07 21:35:23 +0300

    cryptography is no more

--- 02_knight_and_princess/anton_potapov/graph_create.cpp
+++ 02_knight_and_princess/anton_potapov/graph_create.cpp
@@ -34,13 +34,15 @@ class Graph {
   }
   std::string get_json_string() const {
     std::stringstream json_stringstream;
-    json_stringstream << "{" << std::endl;;
+    json_stringstream << "{" << std::endl;
+    ;
 
     json_stringstream << "\t\"vertices\": [" << std::endl;
     for (auto it = adjacency_list_.begin(); it != adjacency_list_.end(); ++it) {
       const auto& [ith_vertex_id, ith_adjacency_list] = *it;
       json_stringstream << "\t\t{" << std::endl;
-      json_stringstream << "\t\t\t\"id\": " << ith_vertex_id << "," << std::endl;
+      json_stringstream << "\t\t\t\"id\": " << ith_vertex_id << ","
+                        << std::endl;
       json_stringstream << "\t\t\t\"edge_ids\": [";
       for (size_t j = 0; j < ith_adjacency_list.size(); ++j) {
         json_stringstream << ith_adjacency_list[j];
@@ -85,17 +87,17 @@ class Graph {
 
 Graph task_02_get_graph() {
   const size_t n = 14;
-  std::vector<Vertex> v_ans;
+  std::vector<Vertex> vertices_ans;
   for (size_t i = 0; i < n; ++i) {
-    v_ans.push_back(Vertex(i));
+    vertices_ans.push_back(Vertex(i));
   }
-  const std::vector<Edge> e_ans{Edge(0, 0, 1),    Edge(1, 0, 2),    Edge(2, 0, 3),
-                          Edge(3, 1, 4),    Edge(4, 1, 5),    Edge(5, 1, 6),
-                          Edge(6, 2, 7),    Edge(7, 2, 8),    Edge(8, 3, 9),
-                          Edge(9, 4, 10),   Edge(10, 5, 10),  Edge(11, 6, 10),
-                          Edge(12, 7, 11),  Edge(13, 8, 11),  Edge(14, 9, 12),
-                          Edge(15, 10, 13), Edge(16, 11, 13), Edge(17, 12, 13)};
-  return Graph(v_ans, e_ans);
+  const std::vector<Edge> edges_ans{
+      Edge(0, 0, 1),    Edge(1, 0, 2),   Edge(2, 0, 3),   Edge(3, 1, 4),
+      Edge(4, 1, 5),    Edge(5, 1, 6),   Edge(6, 2, 7),   Edge(7, 2, 8),
+      Edge(8, 3, 9),    Edge(9, 4, 10),  Edge(10, 5, 10), Edge(11, 6, 10),
+      Edge(12, 7, 11),  Edge(13, 8, 11), Edge(14, 9, 12), Edge(15, 10, 13),
+      Edge(16, 11, 13), Edge(17, 12, 13)};
+  return Graph(vertices_ans, edges_ans);
 }
 
 int main() {
