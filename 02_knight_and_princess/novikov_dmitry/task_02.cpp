0dd5df3be354bb9fae48ca6b084726bd2206ee3b
Dmitry Novikov <dimchanov1121@mail.ru>
2021-10-07 12:54:26 +0300

    changed the default value generator - second version

--- 02_knight_and_princess/novikov_dmitry/task_02.cpp
+++ 02_knight_and_princess/novikov_dmitry/task_02.cpp
@@ -2,7 +2,6 @@
 #include <cassert>
 #include <fstream>
 #include <iostream>
-#include <memory>
 #include <sstream>
 #include <string>
 #include <unordered_map>
@@ -15,9 +14,9 @@ class Vertex {
  public:
   explicit Vertex(const VertexId& init_id = -1) : id_(init_id) {}
 
-  void add_edge_id(EdgeId init_id) { edge_ids_.push_back(init_id); }
+  void add_edge_id(const EdgeId& init_id) { edge_ids_.push_back(init_id); }
 
-  std::string to_str() const {
+  std::string to_string() const {
     std::stringstream ss_out;
     std::string tab_1 = "    ";
     std::string tab_2 = tab_1 + tab_1;
@@ -44,11 +43,13 @@ class Vertex {
 
 class Edge {
  public:
-  Edge() {}
-  Edge(VertexId init_ver_id1, EdgeId init_id, VertexId init_ver_id2)
+  Edge() = default;
+  Edge(const VertexId& init_ver_id1,
+       const EdgeId& init_id,
+       const VertexId& init_ver_id2)
       : ver_id1_(init_ver_id1), id_(init_id), ver_id2_(init_ver_id2) {}
 
-  std::string to_str() const {
+  std::string to_string() const {
     std::stringstream ss_out;
     std::string tab_1 = "    ";
     std::string tab_2 = tab_1 + tab_1;
@@ -70,52 +71,45 @@ class Edge {
 
 class Graph {
  public:
-  using VertexMap = std::unordered_map<VertexId, Vertex>;
-  using EdgeMap = std::unordered_map<EdgeId, Edge>;
-
-  VertexMap vertex_map;
-  EdgeMap edge_map;
-
   Graph() = default;
 
-  void add_vertex(VertexId init_ver_id = default_ver_id_--) {
-    assert(vertex_map.find(init_ver_id) == vertex_map.end() &&
-           "Vertex already exists");
-    vertex_map[init_ver_id] = Vertex(init_ver_id);
+  void add_vertex() {
+    vertex_map_[default_ver_id_] = Vertex(default_ver_id_);
+    ++default_ver_id_;
   }
 
-  void add_edge(VertexId init_ver1_id,
-                EdgeId init_edge_id,
-                VertexId init_ver2_id) {
-    assert(!(vertex_map.find(init_ver1_id) == vertex_map.end()) &&
-           "Vertex already not exists");
-    assert(!(vertex_map.find(init_ver2_id) == vertex_map.end()) &&
-           "Vertex already not exists");
-    edge_map[init_edge_id] = Edge(init_ver1_id, init_edge_id, init_ver2_id);
-    vertex_map[init_ver1_id].add_edge_id(init_edge_id);
-    vertex_map[init_ver2_id].add_edge_id(init_edge_id);
+  void add_edge(const VertexId& init_ver1_id, const VertexId& init_ver2_id) {
+    assert(vertex_map_.find(init_ver1_id) != vertex_map_.end() &&
+           "Vertex doesn't exists");
+    assert(vertex_map_.find(init_ver2_id) != vertex_map_.end() &&
+           "Vertex doesn't exists");
+    edge_map_[default_edge_id_] =
+        Edge(init_ver1_id, default_edge_id_, init_ver2_id);
+    vertex_map_[init_ver1_id].add_edge_id(default_edge_id_);
+    vertex_map_[init_ver2_id].add_edge_id(default_edge_id_);
+    ++default_edge_id_;
   }
 
-  std::string to_str() const {
+  std::string to_string() const {
     std::stringstream ss_out;
     std::string tab_1 = "    ";
 
     ss_out << "{\n";
     ss_out << tab_1 << "\"vertices\": [\n";
-    for (auto it = vertex_map.begin(); it != vertex_map.end(); ++it) {
-      if (it != vertex_map.begin()) {
+    for (auto it = vertex_map_.begin(); it != vertex_map_.end(); ++it) {
+      if (it != vertex_map_.begin()) {
         ss_out << ", ";
       }
-      ss_out << it->second.to_str();
+      ss_out << it->second.to_string();
     }
     ss_out << std::endl << tab_1 << "],\n";
 
     ss_out << tab_1 << "\"edges\": [\n";
-    for (auto it = edge_map.begin(); it != edge_map.end(); ++it) {
-      if (it != edge_map.begin()) {
+    for (auto it = edge_map_.begin(); it != edge_map_.end(); ++it) {
+      if (it != edge_map_.begin()) {
         ss_out << ", ";
       }
-      ss_out << it->second.to_str();
+      ss_out << it->second.to_string();
     }
     ss_out << std::endl << tab_1 << "]\n";
     ss_out << "}" << std::endl;
@@ -123,28 +117,32 @@ class Graph {
   }
 
  private:
-  inline static VertexId default_ver_id_ = -1;
+  VertexId default_ver_id_ = 0;
+  EdgeId default_edge_id_ = 0;
+  using VertexMap = std::unordered_map<VertexId, Vertex>;
+  using EdgeMap = std::unordered_map<EdgeId, Edge>;
+  VertexMap vertex_map_;
+  EdgeMap edge_map_;
 };
 
 int main() {
-  Graph G = Graph();
-  const std::vector<VertexId> verteces = {0, 1, 2, 3,  4,  5,  6,
-                                          7, 8, 9, 10, 11, 12, 13};
-  for (const auto& item : verteces) {
-    G.add_vertex(item);
+  Graph graph = Graph();
+
+  const int verteces_number = 14;
+  for (int i = 0; i < verteces_number; ++i) {
+    graph.add_vertex();
   }
-  const std::vector<std::vector<int> > g_vec = {
-      {0, 0, 1},    {0, 1, 2},    {0, 2, 3},   {1, 3, 4},   {1, 4, 5},
-      {1, 5, 6},    {2, 6, 7},    {2, 7, 8},   {3, 8, 9},   {4, 9, 10},
-      {5, 10, 10},  {6, 11, 10},  {7, 12, 11}, {8, 13, 11}, {9, 14, 12},
-      {10, 15, 13}, {11, 16, 13}, {12, 17, 13}};
-  for (const auto& item : g_vec) {
-    G.add_edge(item[0], item[1], item[2]);
+  const std::vector<std::vector<int> > edge_vec = {
+      {0, 1},  {0, 2},  {0, 3},  {1, 4},   {1, 5},   {1, 6},
+      {2, 7},  {2, 8},  {3, 9},  {4, 10},  {5, 10},  {6, 10},
+      {7, 11}, {8, 11}, {9, 12}, {10, 13}, {11, 13}, {12, 13}};
+  for (const auto& item : edge_vec) {
+    graph.add_edge(item[0], item[1]);
   }
 
   std::ofstream file_out;
   file_out.open("Graph.json", std::fstream::out | std::fstream::trunc);
-  file_out << G.to_str();
+  file_out << graph.to_string();
   file_out.close();
 
   return 0;
