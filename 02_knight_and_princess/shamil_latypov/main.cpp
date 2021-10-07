ccaab5d2a27fab2a53d4f0df884254ca707e0129
2Lapsha1 <2Lapsha1@gmail.com>
2021-10-07 18:36:49 +0300

    fixed clang-format

--- 02_knight_and_princess/shamil_latypov/main.cpp
+++ 02_knight_and_princess/shamil_latypov/main.cpp
@@ -1,108 +1,107 @@
 #include <stdlib.h>
 #include <algorithm>
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <array>
 
 std::ofstream out_json("out.json");
 
 class Vertex {
-public:
-    int id = 0;
-    std::vector<int> edge_ids;
+ public:
+  int id = 0;
+  std::vector<int> edge_ids;
 
-    void to_string() {
-        out_json << "{ \"id\": " << id << ", \"edge_ids\": [";
-        bool check = true;
+  void to_string() {
+    out_json << "{ \"id\": " << id << ", \"edge_ids\": [";
+    bool check = true;
 
-        for (const auto& edge_id : edge_ids) {
-            if (check) {
-                check = false;
-            } else {
-                out_json << ", ";
-            }
-            out_json << edge_id;
-        }
-        out_json << "] }";
+    for (const auto& edge_id : edge_ids) {
+      if (check) {
+        check = false;
+      } else {
+        out_json << ", ";
+      }
+      out_json << edge_id;
     }
-    
-    bool operator< (const Vertex &other) {
-        return id < other.id;
-    }
+    out_json << "] }";
+  }
+
+  bool operator<(const Vertex& other) { return id < other.id; }
 };
 
 class Edge {
-public:
-    int id = 0;
-    Vertex* v1;
-    Vertex* v2;
+ public:
+  int id = 0;
+  Vertex* v1;
+  Vertex* v2;
 
-    Edge (int id_, Vertex* v1_, Vertex* v2_) : id(id_), v1(v1_), v2(v2_) {}
+  Edge(int id_, Vertex* v1_, Vertex* v2_) : id(id_), v1(v1_), v2(v2_) {}
 
-    void to_string() {
-        out_json << "{ \"id\": " << id << ", \"vertex_ids\": [" << v1->id << ", "
-                << v2->id << "] }";
-    }
-    
-    bool operator< (const Edge &other) {
-        return id < other.id;
-    }
+  void to_string() {
+    out_json << "{ \"id\": " << id << ", \"vertex_ids\": [" << v1->id << ", "
+             << v2->id << "] }";
+  }
 
+  bool operator<(const Edge& other) { return id < other.id; }
 };
 
 template <size_t SIZE>
-void output_graph(std::vector<Edge> edge_mas, std::array<Vertex, SIZE> vert_mas, int numedge, int numvert) {
-    out_json << "{\n  \"vertices\": [\n    ";
-    bool check_first_comma = false;
+void output_graph(std::vector<Edge> edge_mas,
+                  std::array<Vertex, SIZE> vert_mas,
+                  int numedge,
+                  int numvert) {
+  out_json << "{\n  \"vertices\": [\n    ";
+  bool check_first_comma = false;
 
-    for (int i = 0; i < numvert; i++) {
-        if (check_first_comma)
-            out_json << ",\n    ";
-        check_first_comma = true;
+  for (int i = 0; i < numvert; i++) {
+    if (check_first_comma)
+      out_json << ",\n    ";
+    check_first_comma = true;
 
-        vert_mas[i].to_string();
-    }
+    vert_mas[i].to_string();
+  }
 
-    out_json << "\n  ],\n  \"edges\": [\n    ";
-    check_first_comma = false;
+  out_json << "\n  ],\n  \"edges\": [\n    ";
+  check_first_comma = false;
 
-    for (int i = 0; i < numedge; i++) {
-        if (check_first_comma)
-            out_json << ",\n    ";
-        check_first_comma = true;
+  for (int i = 0; i < numedge; i++) {
+    if (check_first_comma)
+      out_json << ",\n    ";
+    check_first_comma = true;
 
-        edge_mas[i].to_string();
-    }
-    out_json << "\n  ]\n}\n";
+    edge_mas[i].to_string();
+  }
+  out_json << "\n  ]\n}\n";
 }
 
 int main() {
-    const int numedge = 18, numvertex = 14;
-    std::array<Vertex, numvertex> vert_mas;
-    std::vector<Edge> edge_mas;
-    
-    for (int i = 0; i < numvertex; i++)
-        vert_mas[i].id = i;
-    
-    std::pair<int, int> graph_data[numedge] = {
-        {0, 1},  {0, 2},  {0, 3},  {1, 4},   {1, 5},   {1, 6},
-        {2, 7},  {2, 8},  {3, 9},  {4, 10},  {5, 10},  {6, 10},
-        {7, 11}, {8, 11}, {9, 12}, {10, 13}, {11, 13}, {12, 13}};
+  const int numedge = 18, numvertex = 14;
+  std::array<Vertex, numvertex> vert_mas;
+  std::vector<Edge> edge_mas;
 
-    for (int i = 0; i < numedge; i++) {
-        
-        vert_mas[graph_data[i].first].edge_ids.push_back(i);
-        vert_mas[graph_data[i].second].edge_ids.push_back(i);
+  for (int i = 0; i < numvertex; i++)
+    vert_mas[i].id = i;
 
-        edge_mas.push_back((Edge){i, &vert_mas[graph_data[i].first], &vert_mas[graph_data[i].second]});//[i].v1 = &vert_mas[graph_data[i].first];
-    }
+  std::pair<int, int> graph_data[numedge] = {
+      {0, 1},  {0, 2},  {0, 3},  {1, 4},   {1, 5},   {1, 6},
+      {2, 7},  {2, 8},  {3, 9},  {4, 10},  {5, 10},  {6, 10},
+      {7, 11}, {8, 11}, {9, 12}, {10, 13}, {11, 13}, {12, 13}};
 
-    output_graph(edge_mas, vert_mas, numedge, numvertex);
+  for (int i = 0; i < numedge; i++) {
+    vert_mas[graph_data[i].first].edge_ids.push_back(i);
+    vert_mas[graph_data[i].second].edge_ids.push_back(i);
 
-    out_json.close();
-    return 0;
+    edge_mas.push_back((Edge){
+        i, &vert_mas[graph_data[i].first],
+        &vert_mas[graph_data[i].second]});
+  }
+
+  output_graph(edge_mas, vert_mas, numedge, numvertex);
+
+  out_json.close();
+  return 0;
 }
