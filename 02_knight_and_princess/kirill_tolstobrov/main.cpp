b5f75d88b6d2d206dada6249372317f9650a97ff
lanzy4 <lanzy4@yandex.ru>
2021-10-08 00:18:16 +0300

    Fix task_02

--- 02_knight_and_princess/kirill_tolstobrov/main.cpp
+++ 02_knight_and_princess/kirill_tolstobrov/main.cpp
@@ -9,17 +9,17 @@ using EdgeId = int;
 
 class Vertex {
  public:
-  explicit Vertex(int init_id = 0) : id(init_id) {}
-  int get_id() const { return id; }
-  void add_edge(int edge_id) { connected_edges.push_back(edge_id); }
+  explicit Vertex(VertexId init_id = 0) : ID_(init_id) {}
+  VertexId get_id() const { return ID_; }
+  void add_edge(EdgeId edge_id) { connected_edges_.push_back(edge_id); }
   operator std::string() {
     std::string result = "";
-    result += "    {\n      \"id\": " + std::to_string(id);
+    result += "    {\n      \"id\": " + std::to_string(ID_);
     result += ",\n      \"edge_ids\": [";
 
-    for (int i = 0; i < connected_edges.size(); i++) {
-      result += std::to_string(connected_edges[i]);
-      if (i == connected_edges.size() - 1) {
+    for (int i = 0; i < connected_edges_.size(); i++) {
+      result += std::to_string(connected_edges_[i]);
+      if (i == connected_edges_.size() - 1) {
         result += "]\n";
       } else {
         result += ", ";
@@ -30,70 +30,69 @@ class Vertex {
   }
 
  private:
-  const VertexId id;
-  std::vector<int> connected_edges;
+  const VertexId ID_;
+  std::vector<EdgeId> connected_edges_;
 };
 
 class Edge {
  public:
-  Edge(int init_id, int v1_init, int v2_init)
-      : id(init_id), vertex1_id(v1_init), vertex2_id(v2_init){};
-  int get_id() const { return id; }
-  int get_v1_id() const { return vertex1_id; }
-  int get_v2_id() const { return vertex2_id; }
+  Edge(EdgeId init_id, VertexId v1_init, VertexId v2_init)
+      : ID_(init_id), vertex1_id_(v1_init), vertex2_id_(v2_init){};
+  EdgeId get_id() const { return ID_; }
+  VertexId get_v1_id() const { return vertex1_id_; }
+  VertexId get_v2_id() const { return vertex2_id_; }
   operator std::string() {
     std::string result = "";
-    result += "    {\n      \"id\": " + std::to_string(id);
+    result += "    {\n      \"id\": " + std::to_string(ID_);
     result += ",\n      \"vertex_ids\": ";
 
-    result += "[" + std::to_string(vertex1_id);
-    result += ", " + std::to_string(vertex2_id);
+    result += "[" + std::to_string(vertex1_id_);
+    result += ", " + std::to_string(vertex2_id_);
     result += "]\n    }";
 
     return result;
   }
 
  private:
-  const EdgeId id;
-  int vertex1_id;
-  int vertex2_id;
+  const EdgeId ID_;
+  VertexId vertex1_id_;
+  VertexId vertex2_id_;
 };
 
 class Graph {
  public:
-  Graph() {}
 
-  int get_vertices_amount() const { return vertices.size(); }
-  int get_edges_amount() const { return edges.size(); }
+  int get_vertices_amount() const { return vertices_.size(); }
+  int get_edges_amount() const { return edges_.size(); }
 
-  void add_new_vertex() { vertices.push_back(Vertex(vertices.size())); }
+  void add_new_vertex() { vertices_.push_back(Vertex(vertices_.size())); }
 
-  void bind_vertices(int id1, int id2) {
-    auto& edge = edges.emplace_back(edges.size(), id1, id2);
-    connections_map[id1][edge.get_id()] = id2;
-    connections_map[id2][edge.get_id()] = id1;
-    vertices[id1].add_edge(edge.get_id());
-    vertices[id2].add_edge(edge.get_id());
+  void bind_vertices(VertexId id1, VertexId id2) {
+    const auto& edge = edges_.emplace_back(edges_.size(), id1, id2);
+    connections_map_[id1][edge.get_id()] = id2;
+    connections_map_[id2][edge.get_id()] = id1;
+    vertices_[id1].add_edge(edge.get_id());
+    vertices_[id2].add_edge(edge.get_id());
   }
 
   operator std::string() {
     std::string result = "{\n  \"vertices\": [\n";
 
-    for (int i = 0; i < vertices.size(); i++) {
-      std::string vertex_string = (std::string)vertices[i];
+    for (int i = 0; i < vertices_.size(); i++) {
+      std::string vertex_string = (std::string)vertices_[i];
 
       result += vertex_string;
-      if (i != vertices.size() - 1)
+      if (i != vertices_.size() - 1)
         result += ",\n";
     }
 
     result += "\n  ],\n  \"edges\": [\n";
 
-    for (int i = 0; i < edges.size(); i++) {
-      std::string edge_string = (std::string)edges[i];
+    for (int i = 0; i < edges_.size(); i++) {
+      std::string edge_string = (std::string)edges_[i];
 
       result += edge_string;
-      if (i != edges.size() - 1)
+      if (i != edges_.size() - 1)
         result += ",\n";
     }
 
@@ -102,39 +101,40 @@ class Graph {
   }
 
  private:
-  std::vector<Edge> edges;
-  std::vector<Vertex> vertices;
+  std::vector<Edge> edges_;
+  std::vector<Vertex> vertices_;
 
   // connections_map: vertex1 -> edge -> vertex2
-  std::map<int, std::map<int, int>> connections_map;
+  std::map<VertexId, std::map<EdgeId, VertexId>> connections_map_;
 };
 
-void generateCustomGraph(Graph& g,
-                         int vert_number,
-                         std::vector<std::pair<int, int>> connections) {
+Graph generateCustomGraph(int vert_number,
+                         const std::vector<std::pair<VertexId, VertexId>> &connections) {
+  Graph graph = Graph();
   for (int i = 0; i < vert_number; i++) {
-    g.add_new_vertex();
+    graph.add_new_vertex();
   }
-  for (int i = 0; i < connections.size(); i++) {
-    g.bind_vertices(connections[i].first, connections[i].second);
+  for (const auto& connection : connections) {
+    graph.bind_vertices(connection.first, connection.second);
   }
+  return graph;
 }
 
 int main() {
-  Graph graph = Graph();
 
-  const std::vector<std::pair<int, int>> g_connections{
+  const std::vector<std::pair<VertexId, VertexId>> g_connections{
       {0, 1},  {0, 2},  {0, 3},  {1, 4},   {1, 5},   {1, 6},
       {2, 7},  {2, 8},  {3, 9},  {4, 10},  {5, 10},  {6, 10},
       {7, 11}, {8, 11}, {9, 12}, {10, 13}, {11, 13}, {12, 13}};
 
-  generateCustomGraph(graph, 14, g_connections);
+  Graph graph = generateCustomGraph(14, g_connections);
 
   std::ofstream file;
   file.open("graph.json");
 
   if (file.is_open()) {
     file << (std::string)graph << std::endl;
+    file.close();
   }
 
   return 0;
