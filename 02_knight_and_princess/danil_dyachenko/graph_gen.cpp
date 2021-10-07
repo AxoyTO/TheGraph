2071c2a3121e44c4b087db82be433a1244303f3c
Danil <danil.dyach@gmail.com>
2021-10-07 18:06:58 +0300

    More fixes in task2

--- 02_knight_and_princess/danil_dyachenko/graph_gen.cpp
+++ 02_knight_and_princess/danil_dyachenko/graph_gen.cpp
@@ -4,73 +4,89 @@
 #include <vector>
 
 using VertexId = int;
+using EdgeId = int;
 
 struct Vertex {
   VertexId id;
   std::vector<int> edges;
+
+  std::string& to_json() {
+    static std::string string;
+    string.clear();
+    string += "{ \"id\": ";
+    string += std::to_string(id);
+    string += ", \"edge_ids\": [";
+    for (auto it = edges.begin(); it != edges.end() - 1; it++) {
+      string += std::to_string(*it);
+      string += ", ";
+    }
+    string += std::to_string(*(edges.end() - 1));
+    string += "] }";
+    return string;
+  }
 };
 
 struct Edge {
-  VertexId id;
+  EdgeId id;
   std::array<int, 2> vertices;
+
+  std::string& to_json() {
+    static std::string string;
+    string.clear();
+    string += "{ \"id\": ";
+    string += std::to_string(id);
+    string += ", \"vertex_ids\": [";
+    string += std::to_string(vertices[0]);
+    string += ", ";
+    string += std::to_string(vertices[1]);
+    string += "] }";
+    return string;
+  }
 };
 
 class Graph {
  public:
-  void add_vertices(const std::vector<Vertex>& vec) { vertices_ = vec; }
+  Graph(const std::vector<Vertex>& vertices, const std::vector<Edge>& edges)
+      : vertices_(vertices), edges_(edges) {}
+
   void print_vertices() const {
     std::cout << "Printing vertices:" << std::endl;
-    for (int i = 0; i < vertices_.size(); ++i) {
-      std::cout << i << ": [ ";
-      for (int j = 0; j < vertices_[i].edges.size(); ++j)
-        std::cout << vertices_[i].edges[j] << " ";
+    for (const auto& vertex : vertices_) {
+      std::cout << vertex.id << ": [ ";
+      for (const auto& edge : vertex.edges)
+        std::cout << edge << " ";
       std::cout << "]" << std::endl;
     }
   }
-  void add_edges(const std::vector<Edge>& vec) { edges_ = vec; }
+
   void print_edges() const {
     std::cout << "Printing edges:" << std::endl;
-    for (int i = 0; i < edges_.size(); ++i) {
-      std::cout << i << ": [ ";
-      for (int j = 0; j < edges_[i].vertices.size(); ++j)
-        std::cout << edges_[i].vertices[j] << " ";
+    for (const auto& edge : edges_) {
+      std::cout << edge.id << ": [ ";
+      for (const auto& vertex : edge.vertices)
+        std::cout << vertex << " ";
       std::cout << "]" << std::endl;
     }
   }
+
   std::string to_json() {
     std::string str;
     str += "{\n\t\"vertices\": [\n";
-    for (int i = 0; i < vertices_.size(); ++i) {
-      str += "\t\t{ \"id\": ";
-      str += std::to_string(i);
-      str += ", \"edge_ids\": [";
-      for (int j = 0; j < vertices_[i].edges.size() - 1; ++j) {
-        str += std::to_string(vertices_[i].edges[j]);
-        str += ", ";
-      }
-      str += std::to_string(vertices_[i].edges[vertices_[i].edges.size() - 1]);
-      str += "]";
-      if (i + 1 < vertices_.size())
-        str += " },\n";
-      else
-        str += " }\n";
+    for (auto it = vertices_.begin(); it != vertices_.end(); it++) {
+      str += "\t\t";
+      str += it->to_json();
+      if (it != vertices_.end() - 1)
+        str += ",";
+      str += "\n";
     }
     str += "\t],\n";
     str += "\t\"edges\": [\n";
-    for (int i = 0; i < edges_.size(); ++i) {
-      str += "\t\t{ \"id\": ";
-      str += std::to_string(i);
-      str += ", \"vertex_ids\": [";
-      for (int j = 0; j < edges_[i].vertices.size() - 1; ++j) {
-        str += std::to_string(edges_[i].vertices[j]);
-        str += ", ";
-      }
-      str += std::to_string(edges_[i].vertices[edges_[i].vertices.size() - 1]);
-      str += "]";
-      if (i + 1 < edges_.size())
-        str += " },\n";
-      else
-        str += " }\n";
+    for (auto it = edges_.begin(); it != edges_.end(); it++) {
+      str += "\t\t";
+      str += it->to_json();
+      if (it != edges_.end() - 1)
+        str += ",";
+      str += "\n";
     }
     str += "\t]\n}\n";
     return str;
@@ -81,46 +97,26 @@ class Graph {
   std::vector<Edge> edges_;
 };
 
-void write_file(const std::string& str, std::string name) {
+void write_file(const std::string& string, const std::string& name) {
   std::ofstream file;
   file.open(name, std::ios::out);
-  file << str;
+  file << string;
 }
 
 int main(void) {
-  Graph my_graph;
-  my_graph.add_vertices((std::vector<Vertex>){{0, {0, 1, 2}},
-                                              {1, {0, 3, 4, 5}},
-                                              {2, {1, 6, 7}},
-                                              {3, {2, 8}},
-                                              {4, {3, 9}},
-                                              {5, {4, 10}},
-                                              {6, {5, 11}},
-                                              {7, {6, 12}},
-                                              {8, {7, 13}},
-                                              {9, {8, 14}},
-                                              {10, {9, 10, 11, 15}},
-                                              {11, {12, 13, 16}},
-                                              {12, {14, 17}},
-                                              {13, {15, 16, 17}}});
-  my_graph.add_edges((std::vector<Edge>){{0, {0, 1}},
-                                         {1, {0, 2}},
-                                         {2, {0, 3}},
-                                         {3, {1, 4}},
-                                         {4, {1, 5}},
-                                         {5, {1, 6}},
-                                         {6, {2, 7}},
-                                         {7, {2, 8}},
-                                         {8, {3, 9}},
-                                         {9, {4, 10}},
-                                         {10, {5, 10}},
-                                         {11, {6, 10}},
-                                         {12, {7, 11}},
-                                         {13, {8, 11}},
-                                         {14, {9, 12}},
-                                         {15, {10, 13}},
-                                         {16, {11, 13}},
-                                         {17, {12, 13}}});
+  const std::vector<Vertex> vertices = {
+      {0, {0, 1, 2}}, {1, {0, 3, 4, 5}},     {2, {1, 6, 7}},
+      {3, {2, 8}},    {4, {3, 9}},           {5, {4, 10}},
+      {6, {5, 11}},   {7, {6, 12}},          {8, {7, 13}},
+      {9, {8, 14}},   {10, {9, 10, 11, 15}}, {11, {12, 13, 16}},
+      {12, {14, 17}}, {13, {15, 16, 17}}};
+  const std::vector<Edge> edges = {
+      {0, {0, 1}},    {1, {0, 2}},   {2, {0, 3}},   {3, {1, 4}},
+      {4, {1, 5}},    {5, {1, 6}},   {6, {2, 7}},   {7, {2, 8}},
+      {8, {3, 9}},    {9, {4, 10}},  {10, {5, 10}}, {11, {6, 10}},
+      {12, {7, 11}},  {13, {8, 11}}, {14, {9, 12}}, {15, {10, 13}},
+      {16, {11, 13}}, {17, {12, 13}}};
+  auto my_graph = Graph(vertices, edges);
   write_file(my_graph.to_json(), "my_graph.json");
   return 0;
 }
