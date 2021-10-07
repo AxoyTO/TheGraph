2910a8d17cd78551b983e7beae7ed143d120eb59
alexmeshr <mao2501@rambler.ru>
2021-10-07 10:22:27 +0300

    update

--- 02_knight_and_princess/alexey_mesheryakov/graph.cpp
+++ 02_knight_and_princess/alexey_mesheryakov/graph.cpp
@@ -41,7 +41,7 @@ public:
 	
 	//Graph(){};
 
-	void set_params(const vector<Edge>& new_edges, const vector<int>& vert_ids) {
+	void set_params(const vector<Edge>& new_edges = {}, const vector<int>& vert_ids = {}) {
 		for (const auto& id: vert_ids)
 			set_vert({id,{}}); 
 		for (const auto& edge: new_edges)
@@ -85,7 +85,7 @@ int main() {
 	vector <int> info(LEN);
 	for (int i = 0; i < LEN; i++)
 		info[i] = i;
-	vector <Graph::Edge> edges = {{0,{0,1}}, {1,{0,2}}, {2,{0,3}}, {3,{1,4}}, {4,{1,5}}, {5,{1,6}}, {6,{2,7}},
+	const vector <Graph::Edge> edges = {{0,{0,1}}, {1,{0,2}}, {2,{0,3}}, {3,{1,4}}, {4,{1,5}}, {5,{1,6}}, {6,{2,7}},
 		{7,{2,8}}, {8,{3,9}}, {9,{4,10}}, {10,{5,10}}, {11,{6,10}}, {12,{7,11}}, {13,{8,11}}, {14,{9,12}}, {15,{10,13}}, {16,{11,13}}, {17,{12,13}}};
 	Graph graph;
 	graph.set_params(edges, info);
