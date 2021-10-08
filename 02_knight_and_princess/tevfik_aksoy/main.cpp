48edd6c3921467175bdc979a363028b9e7ad405f
Tevfik Oguzhan AKSOY <toaxoy@protonmail.com>
2021-10-08 13:29:16 +0300

    Commit to save process, changes in code

--- 02_knight_and_princess/tevfik_aksoy/main.cpp
+++ 02_knight_and_princess/tevfik_aksoy/main.cpp
@@ -1,4 +1,3 @@
-#include <array>
 #include <fstream>
 #include <iostream>
 #include <set>
@@ -6,7 +5,6 @@
 #include <tuple>
 #include <vector>
 
-using std::array;
 using std::endl;
 using std::get;
 using std::to_string;
@@ -16,13 +14,15 @@ using VertexId = int;
 using EdgeId = int;
 
 // Tuple of <int,int> for realization of vector of vectors of <int,int>
-using dest_id_edge_id = std::tuple<int, int>;
+using dest_id_edge_id = std::tuple<VertexId, EdgeId>;
+
+constexpr int INVALID_ID = -1;
 
 struct Vertex {
  public:
-  VertexId id = 0;
+  VertexId id = INVALID_ID;
 
-  explicit Vertex(VertexId _id = 0) : id(_id) {}
+  explicit Vertex(VertexId _id) : id(_id) {}
 
   std::string to_JSON(const vector<int>& edge_ids) const {
     std::string json_string;
@@ -39,11 +39,11 @@ struct Vertex {
 
 struct Edge {
  public:
-  EdgeId id = 0;
+  EdgeId id = INVALID_ID;
   VertexId source;
   VertexId destination;
 
-  Edge(int src_id, int dest_id, int _id)
+  Edge(VertexId src_id, VertexId dest_id, EdgeId _id)
       : id(_id), source(src_id), destination(dest_id) {}
 
   std::string to_JSON() const {
@@ -57,20 +57,18 @@ struct Edge {
 class Graph {
  public:
   vector<Edge> edges;
-  array<Vertex, 13> vertex;
-  vector<vector<dest_id_edge_id>> vertices;
-
-  Graph() {}
+  vector<Vertex> vertices;
+  vector<vector<dest_id_edge_id>> data;
 
   std::string to_JSON() const {
     vector<int> edge_ids;
     std::string json_string;
     json_string += "{\n\"vertices\": [\n";
     for (int i = 0; i < vertices.size(); i++) {
-      for (const auto& j : vertices[i]) {
+      for (const auto& j : data[i]) {
         edge_ids.push_back(get<1>(j));
       }
-      json_string += vertex[i].to_JSON(edge_ids);
+      json_string += vertices[i].to_JSON(edge_ids);
       if (i + 1 == vertices.size()) {
         json_string += "] }\n  ],\n";
       } else {
@@ -95,42 +93,39 @@ class Graph {
 
 const Graph generateGraph() {
   Graph graph;
-  const array<Edge, 18> edges = {{{0, 1, 0},
-                                  {0, 2, 1},
-                                  {0, 3, 2},
-                                  {1, 4, 3},
-                                  {1, 5, 4},
-                                  {1, 6, 5},
-                                  {2, 7, 6},
-                                  {2, 8, 7},
-                                  {3, 9, 8},
-                                  {4, 10, 9},
-                                  {5, 10, 10},
-                                  {6, 10, 11},
-                                  {7, 11, 12},
-                                  {8, 11, 13},
-                                  {9, 12, 14},
-                                  {10, 13, 15},
-                                  {11, 13, 16},
-                                  {12, 13, 17}}};
+  const vector<Edge> edges = {{{0, 1, 0},
+                               {0, 2, 1},
+                               {0, 3, 2},
+                               {1, 4, 3},
+                               {1, 5, 4},
+                               {1, 6, 5},
+                               {2, 7, 6},
+                               {2, 8, 7},
+                               {3, 9, 8},
+                               {4, 10, 9},
+                               {5, 10, 10},
+                               {6, 10, 11},
+                               {7, 11, 12},
+                               {8, 11, 13},
+                               {9, 12, 14},
+                               {10, 13, 15},
+                               {11, 13, 16},
+                               {12, 13, 17}}};
   std::set<int> nodes_set;
   for (const auto& e : edges) {
     nodes_set.insert(e.source);
+    nodes_set.insert(e.destination);
   }
-  for (int i = 0; i < nodes_set.size() + 1; i++)
-    graph.vertex[i].id = i;
+  for (int i = 0; i < nodes_set.size(); i++)
+    graph.vertices.push_back(Vertex(i));
 
-  graph.vertices.resize(nodes_set.size() + 1);
+  graph.data.resize(nodes_set.size() + 1);
   for (const auto& edge : edges) {
-    const int src_id = edge.source;
-    if (src_id >= graph.vertices.size())
-      continue;
-    const int dest_id = edge.destination;
-    const int edge_id = edge.id;
-
-    graph.edges.emplace_back(src_id, dest_id, edge_id);
-    graph.vertices[src_id].push_back(std::make_tuple(dest_id, edge_id));
-    graph.vertices[dest_id].push_back(std::make_tuple(src_id, edge_id));
+    graph.edges.emplace_back(edge.source, edge.destination, edge.id);
+    graph.data[edge.source].push_back(
+        std::make_tuple(edge.destination, edge.id));
+    graph.data[edge.destination].push_back(
+        std::make_tuple(edge.source, edge.id));
   }
   return graph;
 }
