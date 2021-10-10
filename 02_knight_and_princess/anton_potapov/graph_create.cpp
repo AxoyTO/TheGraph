f0271f57186121ae6a283eba6d9142e26f8bd506
Flagro <antonpgrey@gmail.com>
2021-10-11 01:30:58 +0300

    changed initializing&containing graph logic + classes exports as json strings

--- 02_knight_and_princess/anton_potapov/graph_create.cpp
+++ 02_knight_and_princess/anton_potapov/graph_create.cpp
@@ -2,112 +2,136 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <set>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 using VertexId = int;
 using EdgeId = int;
 
-struct Vertex {
-  VertexId id;
-  explicit Vertex(const VertexId& vertex_id) : id(vertex_id) {}
+class Vertex {
+ public:
+  explicit Vertex(const VertexId& vertex_id) : id_(vertex_id) {}
+  std::string get_json_string() const {
+    std::stringstream json_stringstream;
+    json_stringstream << "{\"id\":" << id_ << ","
+                      << "\"edge_ids\":[";
+    for (auto it = adjacted_edges_.begin(); it != adjacted_edges_.end(); ++it) {
+      json_stringstream << *it;
+      if (std::next(it) != adjacted_edges_.end()) {
+        json_stringstream << ",";
+      }
+    }
+    json_stringstream << "]}";
+    return json_stringstream.str();
+  }
+  void add_edge(const EdgeId& edge_id) { adjacted_edges_.insert(edge_id); }
+
+ private:
+  const VertexId id_;
+  std::set<EdgeId> adjacted_edges_;
 };
 
-struct Edge {
-  EdgeId id;
-  VertexId vertex1_id, vertex2_id;
+class Edge {
+ public:
   Edge(const EdgeId& edge_id, const VertexId& vertex1, const VertexId& vertex2)
-      : id(edge_id), vertex1_id(vertex1), vertex2_id(vertex2) {}
+      : id_(edge_id), vertex1_id_(vertex1), vertex2_id_(vertex2) {}
+  std::string get_json_string() const {
+    std::stringstream json_stringstream;
+    json_stringstream << "{\"id\":" << id_ << ","
+                      << "\"vertex_ids\":[" << vertex1_id_ << "," << vertex2_id_
+                      << "]}";
+    return json_stringstream.str();
+  }
+
+ private:
+  const EdgeId id_;
+  const VertexId vertex1_id_, vertex2_id_;
 };
 
 class Graph {
  public:
   Graph() = default;
-  Graph(const std::vector<Vertex>& vertices, const std::vector<Edge>& edges)
-      : vertices_(vertices), edges_(edges) {
-    for (const auto& edge : edges) {
-      bool first_vertex_is_found = false, second_vertex_is_found = false;
-      for (const auto& vertex : vertices) {
-        if (vertex.id == edge.vertex1_id) {
-          first_vertex_is_found = true;
-        }
-        if (vertex.id == edge.vertex2_id) {
-          second_vertex_is_found = true;
-        }
-      }
-      assert(first_vertex_is_found && second_vertex_is_found &&
-             "Graph constructor: given graph is invalid");
-      adjacency_list_[edge.vertex1_id].push_back(edge.id);
-      adjacency_list_[edge.vertex2_id].push_back(edge.id);
-    }
+
+  VertexId add_vertex() {
+    VertexId new_vertex_id = next_vertex_id_;
+    update_next_vertex_id_();
+    Vertex new_vertex(new_vertex_id);
+    vertices_.insert({new_vertex_id, new_vertex});
+    return new_vertex_id;
   }
+
+  EdgeId add_edge(VertexId vertex1, VertexId vertex2) {
+    EdgeId new_edge_id = next_edge_id_;
+    update_next_edge_id_();
+    Edge new_edge(new_edge_id, vertex1, vertex2);
+    if (vertices_.find(vertex1) == vertices_.end()) {
+      throw std::invalid_argument(
+          "first vertex does not occur in current state of the graph");
+    } else {
+      auto it = vertices_.find(vertex1);
+      it->second.add_edge(new_edge_id);
+    }
+    if (vertices_.find(vertex2) == vertices_.end()) {
+      throw std::invalid_argument(
+          "second vertex does not occur in current state of the graph");
+    } else {
+      auto it = vertices_.find(vertex2);
+      it->second.add_edge(new_edge_id);
+    }
+    edges_.insert({new_edge_id, new_edge});
+    return new_edge_id;
+  }
+
   std::string get_json_string() const {
     std::stringstream json_stringstream;
-    json_stringstream << "{" << std::endl;
-    ;
-
-    json_stringstream << "\t\"vertices\": [" << std::endl;
-    for (auto it = adjacency_list_.begin(); it != adjacency_list_.end(); ++it) {
-      const auto& [ith_vertex_id, ith_adjacency_list] = *it;
-      json_stringstream << "\t\t{" << std::endl;
-      json_stringstream << "\t\t\t\"id\": " << ith_vertex_id << ","
-                        << std::endl;
-      json_stringstream << "\t\t\t\"edge_ids\": [";
-      for (size_t j = 0; j < ith_adjacency_list.size(); ++j) {
-        json_stringstream << ith_adjacency_list[j];
-        if (j + 1 != ith_adjacency_list.size()) {
-          json_stringstream << ", ";
-        }
-      }
-      json_stringstream << "]" << std::endl;
-      json_stringstream << "\t\t}";
-      if (std::next(it) == adjacency_list_.end()) {
-        json_stringstream << std::endl;
-      } else {
-        json_stringstream << "," << std::endl;
+    json_stringstream << "{\"vertices\":[";
+    for (auto it = vertices_.begin(); it != vertices_.end(); ++it) {
+      json_stringstream << it->second.get_json_string();
+      if (std::next(it) != vertices_.end()) {
+        json_stringstream << ",";
       }
     }
-    json_stringstream << "\t],";
-
-    json_stringstream << " \"edges\": [" << std::endl;
-    for (size_t i = 0; i < edges_.size(); ++i) {
-      json_stringstream << "\t\t{" << std::endl;
-      json_stringstream << "\t\t\t\"id\": " << edges_[i].id << "," << std::endl;
-      json_stringstream << "\t\t\t\"vertex_ids\": [" << edges_[i].vertex1_id
-                        << ", " << edges_[i].vertex2_id << "]" << std::endl;
-      json_stringstream << "\t\t}";
-      if (i + 1 == edges_.size()) {
-        json_stringstream << std::endl;
-      } else {
-        json_stringstream << "," << std::endl;
+    json_stringstream << "],\"edges\":[";
+    for (auto it = edges_.begin(); it != edges_.end(); ++it) {
+      json_stringstream << it->second.get_json_string();
+      if (std::next(it) != edges_.end()) {
+        json_stringstream << ",";
       }
     }
-    json_stringstream << "\t]" << std::endl;
-
-    json_stringstream << "}" << std::endl;
+    json_stringstream << "]}";
     return json_stringstream.str();
   }
 
  private:
-  std::vector<Vertex> vertices_;
-  std::vector<Edge> edges_;
-  std::map<VertexId, std::vector<VertexId>> adjacency_list_;
+  VertexId next_vertex_id_{};
+  EdgeId next_edge_id_{};
+  std::map<VertexId, Vertex> vertices_;
+  std::map<EdgeId, Edge> edges_;
+  void update_next_vertex_id_() { ++next_vertex_id_; }
+  void update_next_edge_id_() { ++next_edge_id_; }
 };
 
 Graph task_02_get_graph() {
+  Graph task_02_graph;
   const size_t n = 14;
-  std::vector<Vertex> vertices_ans;
+  std::vector<VertexId> added_vertices;
   for (size_t i = 0; i < n; ++i) {
-    vertices_ans.push_back(Vertex(i));
+    auto new_vertex = task_02_graph.add_vertex();
+    added_vertices.push_back(new_vertex);
   }
-  const std::vector<Edge> edges_ans{
-      Edge(0, 0, 1),    Edge(1, 0, 2),   Edge(2, 0, 3),   Edge(3, 1, 4),
-      Edge(4, 1, 5),    Edge(5, 1, 6),   Edge(6, 2, 7),   Edge(7, 2, 8),
-      Edge(8, 3, 9),    Edge(9, 4, 10),  Edge(10, 5, 10), Edge(11, 6, 10),
-      Edge(12, 7, 11),  Edge(13, 8, 11), Edge(14, 9, 12), Edge(15, 10, 13),
-      Edge(16, 11, 13), Edge(17, 12, 13)};
-  return Graph(vertices_ans, edges_ans);
+  const std::vector<std::pair<size_t, size_t>> edge_vertices_indexes{
+      {0, 1},  {0, 2},  {0, 3},  {1, 4},   {1, 5},   {1, 6},
+      {2, 7},  {2, 8},  {3, 9},  {4, 10},  {5, 10},  {6, 10},
+      {7, 11}, {8, 11}, {9, 12}, {10, 13}, {11, 13}, {12, 13}};
+  for (const auto& edge : edge_vertices_indexes) {
+    VertexId vertex1 = added_vertices[edge.first];
+    VertexId vertex2 = added_vertices[edge.second];
+    task_02_graph.add_edge(vertex1, vertex2);
+  }
+  return task_02_graph;
 }
 
 int main() {
@@ -116,7 +140,9 @@ int main() {
 
   std::fstream json_file;
   json_file.open("graph.json", std::ios::out);
-  assert(json_file && "file not created");
+  if (!json_file) {
+    throw std::runtime_error("file not created");
+  }
   json_file << json_string;
   json_file.close();
   return 0;
