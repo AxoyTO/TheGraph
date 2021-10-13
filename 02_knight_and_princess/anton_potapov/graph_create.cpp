a6172c369fb274e6b745d454aa88628fb40874b0
Flagro <antonpgrey@gmail.com>
2021-10-13 23:50:11 +0300

    optimized is_connected method

--- 02_knight_and_princess/anton_potapov/graph_create.cpp
+++ 02_knight_and_princess/anton_potapov/graph_create.cpp
@@ -13,14 +13,15 @@ using EdgeId = int;
 
 class Vertex {
  public:
+  std::set<EdgeId> connected_edges;
   explicit Vertex(const VertexId& vertex_id) : id_(vertex_id) {}
   std::string get_json_string() const {
     std::stringstream json_stringstream;
     json_stringstream << "{\"id\":" << id_ << ","
                       << "\"edge_ids\":[";
-    for (auto it = adjacted_edges_.begin(); it != adjacted_edges_.end(); ++it) {
+    for (auto it = connected_edges.begin(); it != connected_edges.end(); ++it) {
       json_stringstream << *it;
-      if (std::next(it) != adjacted_edges_.end()) {
+      if (std::next(it) != connected_edges.end()) {
         json_stringstream << ",";
       }
     }
@@ -28,16 +29,15 @@ class Vertex {
     return json_stringstream.str();
   }
   bool has_edge_id(const EdgeId& edge_id) const {
-    return adjacted_edges_.find(edge_id) != adjacted_edges_.end();
+    return connected_edges.find(edge_id) != connected_edges.end();
   }
   void add_edge(const EdgeId& edge_id) {
     assert(!has_edge_id(edge_id) && "edge that is to be added already exists");
-    adjacted_edges_.insert(edge_id);
+    connected_edges.insert(edge_id);
   }
 
  private:
   const VertexId id_;
-  std::set<EdgeId> adjacted_edges_;
 };
 
 class Edge {
@@ -64,11 +64,12 @@ class Graph {
   }
 
   bool is_connected(const VertexId& vertex1, const VertexId& vertex2) const {
-    for (const auto& edge : edges_) {
-      if ((edge.second.vertex1_id == vertex1 &&
-           edge.second.vertex2_id == vertex2) ||
-          (edge.second.vertex1_id == vertex2 &&
-           edge.second.vertex2_id == vertex1)) {
+    assert(is_vertex_exists(vertex1) && "Vertex 1 doesn't exist");
+    assert(is_vertex_exists(vertex2) && "Vertex 2 doesn't exist");
+    auto it_vertex1 = vertices_.find(vertex1);
+    for (const auto& vertex1_edge : it_vertex1->second.connected_edges) {
+      auto it_vertex2 = vertices_.find(vertex2);
+      if (it_vertex2->second.has_edge_id(vertex1_edge)) {
         return true;
       }
     }
