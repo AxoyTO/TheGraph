9293c8a12e0f22c82d35874596f8ab858b9ec60a
Gleb <skryabin.gleb.d@gmail.com>
2021-10-14 15:56:35 +0300

    Edge & Vertex classes; json writer

--- 02_knight_and_princess/gleb_skryabin/graph.hpp
+++ 02_knight_and_princess/gleb_skryabin/graph.hpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <iostream>
 #include <unordered_map>
 #include <unordered_set>
 
@@ -6,80 +7,120 @@ using EdgeId = int;
 using VertexId = int;
 constexpr int INVALID_ID = -1;
 
+class Edge {
+ public:
+  Edge(int inpId, std::pair<VertexId, VertexId> inpVertexIds) {
+    id = inpId;
+    vertexIds.first = inpVertexIds.first;
+    vertexIds.second = inpVertexIds.second;
+  }
+
+  std::string toJSON() {
+    std::string json;
+    json += "{\n      \"id\": " + std::to_string(id);
+    json += ",\n      \"vertex_ids\": [";
+    json += std::to_string(vertexIds.first) + ", ";
+    json += std::to_string(vertexIds.second) + "]\n    }";
+    return json;
+  }
+
+  EdgeId id = INVALID_ID;
+  std::pair<VertexId, VertexId> vertexIds = {INVALID_ID, INVALID_ID};
+};
+
+class Vertex {
+ public:
+  Vertex(int inpId, EdgeId inpEdgeId) {
+    id = inpId;
+    addEdge(inpEdgeId);
+  }
+
+  void addEdge(EdgeId inpEdgeId) { edgeIds.insert(inpEdgeId); }
+
+  std::string toJSON() {
+    std::string json;
+    json += "{\n      \"id\": " + std::to_string(id);
+    json += ",\n      \"edge_ids\": [";
+
+    for (auto pEdgeId = edgeIds.begin(); pEdgeId != edgeIds.end(); pEdgeId++) {
+      json += pEdgeId != edgeIds.begin() ? ", " : "";
+      json += std::to_string(*pEdgeId);
+    }
+
+    json += "]\n    }";
+    return json;
+  }
+
+  EdgeId id = INVALID_ID;
+  std::unordered_set<EdgeId> edgeIds = {};
+};
+
 class Graph {
  public:
-  struct Edge {
+  // структура ребра для входных данных
+  struct InputEdge {
     EdgeId id = INVALID_ID;
-    std::pair<VertexId, VertexId> verticeIds;
+    std::pair<VertexId, VertexId> vertexIds;
   };
 
-  struct Vertex {
+  // структура вершины для входных данных
+  // (пока не используется)
+  struct InputVertex {
     VertexId id = INVALID_ID;
     std::unordered_set<EdgeId> edgeIds;
   };
 
   template <std::size_t SIZE>
-  explicit Graph(const std::array<Edge, SIZE>& inpEdges) {
-    for (auto newEdge : inpEdges) {
-      // изначально edges_ пустой, так что надо добавлять каждый элемент
-      edges_.emplace(newEdge.id, newEdge);
+  explicit Graph(const std::array<InputEdge, SIZE>& inpEdges) {
+    for (InputEdge inpEdge : inpEdges) {
+      addEdge(inpEdge);
     }
-    for (auto edge : edges_) {
-      auto vs = edge.second.verticeIds;
+    for (auto [edgeId, edge] : edges_) {
+      auto vs = edge.vertexIds;
       for (VertexId vsid : std::array<VertexId, 2>{vs.first, vs.second}) {
-        auto v = vertices_.find(vsid);
-        if (v == vertices_.end()) {
-          // add vertex
-          Vertex newVertex = {.id = vsid, .edgeIds = {edge.first}};
-          vertices_.emplace(vsid, newVertex);
-        } else {
-          // edit vertex
-          v->second.edgeIds.insert(edge.first);
-        }
+        compliteVertex(vsid, edgeId);
       }
+    }
+  }
+
+  void addEdge(InputEdge newEdge) {
+    auto edge = edges_.find(newEdge.id);
+    if (edge == edges_.end()) {
+      // add new edge
+      edges_.emplace(newEdge.id, Edge(newEdge.id, newEdge.vertexIds));
+    } else {
+      // edit edge
+      // edge->second.vertexIds = newEdge.vertexIds;
+      std::cout << "warning: edge with id " << newEdge.id
+                << " is already in the graph.\n";
+    }
+  }
+
+  void compliteVertex(VertexId vertexId, EdgeId edgeId) {
+    auto v = vertices_.find(vertexId);
+    if (v == vertices_.end()) {
+      // add new vertex
+      vertices_.emplace(vertexId, Vertex(vertexId, edgeId));
+    } else {
+      // edit vertex
+      v->second.addEdge(edgeId);
     }
   }
 
   std::string toJSON() {
     std::string json;
     json = "{\n  \"vertices\": [\n    ";
-    // я не использую все возможности for, так как мне
-    // все равно придется сравнивать итератор с vertices_.end()
-    for (auto pVertexPair = vertices_.begin();;) {
-      auto v = pVertexPair->second;
-      json += "{\n      \"id\": " + std::to_string(v.id);
-      json += ",\n      \"edge_ids\": [";
-
-      for (auto pEdgeId = v.edgeIds.begin();;) {
-        json += std::to_string(*pEdgeId);
-        if (++pEdgeId != v.edgeIds.end()) {
-          json += ", ";
-        } else {
-          break;
-        }
-      }
-      json += "]\n    }";
-      if (++pVertexPair != vertices_.end()) {
-        json += ", ";
-      } else {
-        break;
-      }
+    for (auto pVertexPair = vertices_.begin(); pVertexPair != vertices_.end();
+         pVertexPair++) {
+      json += pVertexPair != vertices_.begin() ? ", " : "";
+      json += pVertexPair->second.toJSON();
     }
 
     json += "\n  ],\n  \"edges\": [\n    ";
-    for (auto pEdgePair = edges_.begin();;) {
-      EdgeId edgeId = pEdgePair->first;
-      auto vs = pEdgePair->second.verticeIds;
-      json += "{\n      \"id\": " + std::to_string(edgeId);
-      json += ",\n      \"vertex_ids\": [";
-      json += std::to_string(vs.first) + ", ";
-      json += std::to_string(vs.second) + "]\n    }";
-
-      if (++pEdgePair != edges_.end()) {
-        json += ", ";
-      } else {
-        break;
-      }
+    for (auto pEdgePair = edges_.begin(); pEdgePair != edges_.end();
+         pEdgePair++) {
+      json += pEdgePair != edges_.begin() ? ", " : "";
+      json += pEdgePair->second.toJSON();
     }
 
     json += "\n  ]\n}\n";
--- 02_knight_and_princess/gleb_skryabin/main.cpp
+++ 02_knight_and_princess/gleb_skryabin/main.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 #include "graph.hpp"
 
-void writeGraphJSON(Graph g, char const* filename) {
+void writeGraphJSON(Graph graph, char const* filename) {
   std::ofstream json(filename);
   if (json.is_open()) {
-    json << g.toJSON();
+    json << graph.toJSON();
     json.close();
     std::cout << "Graph is written to " << filename << std::endl;
   } else {
@@ -15,26 +15,15 @@ void writeGraphJSON(Graph g, char const* filename) {
 }
 
 int main() {
-  const std::array<Graph::Edge, 18> edges = {{{0, {0, 1}},
-                                              {1, {0, 2}},
-                                              {2, {0, 3}},
-                                              {3, {1, 4}},
-                                              {4, {1, 5}},
-                                              {5, {1, 6}},
-                                              {6, {2, 7}},
-                                              {7, {2, 8}},
-                                              {8, {3, 9}},
-                                              {9, {4, 10}},
-                                              {10, {5, 10}},
-                                              {11, {6, 10}},
-                                              {12, {7, 11}},
-                                              {13, {8, 11}},
-                                              {14, {9, 12}},
-                                              {15, {10, 13}},
-                                              {16, {11, 13}},
-                                              {17, {12, 13}}}};
+  const std::array<Graph::InputEdge, 19> inputEdges = {{
+      {0, {0, 1}},    {1, {0, 2}},    {2, {0, 3}},      {3, {1, 4}},
+      {4, {1, 5}},    {5, {1, 6}},    {6, {2, 7}},      {7, {2, 8}},
+      {8, {3, 9}},    {9, {4, 10}},   {10, {5, 10}},    {11, {6, 10}},
+      {12, {7, 11}},  {13, {8, 11}},  {14, {9, 12}},    {15, {10, 13}},
+      {16, {11, 13}}, {17, {12, 13}}, {13, {111, 111}},
+  }};
 
-  Graph g(edges);
-  writeGraphJSON(g, "graph.json");
+  const Graph graph(inputEdges);
+  writeGraphJSON(graph, "graph.json");
   return 0;
 }
