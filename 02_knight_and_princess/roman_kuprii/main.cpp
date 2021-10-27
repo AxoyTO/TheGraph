1b4b351c32e9fe96cdf7520799b95f55d277f04a
roman <roman.kupry@gmail.com>
2021-10-27 17:14:15 +0300

    fixes

--- 02_knight_and_princess/roman_kuprii/main.cpp
+++ 02_knight_and_princess/roman_kuprii/main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 
 using std::endl;
+using std::min;
 using std::to_string;
 using std::vector;
 
@@ -15,25 +16,53 @@ using VertexId = int;
 
 constexpr int COLORS_NUMBER = 5;
 constexpr int GRAPH_NUMBER = 10;
-constexpr int START_VERTICES_NUMBER = 10;
 constexpr int INVALID_ID = -1;
-constexpr int INVALID_NUMBER = -1;
-const std::string JSON_GRAPH_FILENAME = "temp/graph";
+constexpr int INVALID_NEW_DEPTH = -1;
+constexpr int INVALID_NEW_VERTICES_NUM = -1;
+const std::string JSON_GRAPH_FILENAME = "graph.json";
 const std::string LOG_FILENAME = "log.txt";
 
-int min(const int& a, const int& b) {
-  return (a < b) ? a : b;
-}
+enum class Color { Gray, Green, Blue, Yellow, Red };
 
-enum class Color { GRAY, GREEN, BLUE, YELLOW, RED };
+std::string color_to_string(const Color& color) {
+  std::string res = "";
+  switch (color) {
+    case Color::Gray: {
+      res += "\"gray\" }";
+      break;
+    }
+    case Color::Green: {
+      res += "\"green\" }";
+      break;
+    }
+    case Color::Blue: {
+      res += "\"blue\" }";
+      break;
+    }
+    case Color::Yellow: {
+      res += "\"yellow\" }";
+      break;
+    }
+    case Color::Red: {
+      res += "\"red\" }";
+      break;
+    }
+    default:
+      break;
+  }
+  return res;
+}
 
 struct Edge {
   const EdgeId id = INVALID_ID;
   const std::array<VertexId, 2> connected_vertices;
-  Color color = Color::GRAY;
+  const Color color = Color::Gray;
 
-  explicit Edge(const VertexId& start, const VertexId& end, const EdgeId& _id)
-      : id(_id), connected_vertices({start, end}) {}
+  Edge(const VertexId& start,
+       const VertexId& end,
+       const EdgeId& _id,
+       const Color& _color)
+      : id(_id), connected_vertices({start, end}), color(_color) {}
 
   std::string to_json() const {
     std::string res;
@@ -44,34 +73,9 @@ struct Edge {
     res += ", ";
     res += to_string(connected_vertices[1]);
     res += "], \"color\": ";
-    switch (color) {
-      case Color::GRAY: {
-        res += "\"gray\" }";
-        break;
-      }
-      case Color::GREEN: {
-        res += "\"green\" }";
-        break;
-      }
-      case Color::BLUE: {
-        res += "\"blue\" }";
-        break;
-      }
-      case Color::YELLOW: {
-        res += "\"yellow\" }";
-        break;
-      }
-      case Color::RED: {
-        res += "\"red\" }";
-        break;
-      }
-      default:
-        break;
-    }
+    res += color_to_string(color);
     return res;
   }
-
-  void paint(const Color& _color) { color = _color; }
 };
 
 bool is_edge_id_included(const EdgeId& id, const vector<EdgeId>& edge_ids) {
@@ -155,73 +159,60 @@ class Graph {
     const auto& from_vertex_edges_ids =
         vertices_[from_vertex_id].get_edges_ids();
     const auto& to_vertex_edges_ids = vertices_[to_vertex_id].get_edges_ids();
-    for (const auto& from_vertex_edge_id : from_vertex_edges_ids) {
-      for (const auto& to_vertex_edge_id : to_vertex_edges_ids) {
-        if (from_vertex_edge_id == to_vertex_edge_id)
+    for (const auto& from_vertex_edge_id : from_vertex_edges_ids)
+      if (from_vertex_id == to_vertex_id) {
+        const auto& connected_vertices =
+            edges_[from_vertex_edge_id].connected_vertices;
+        if (connected_vertices[0] == connected_vertices[1])
           return true;
-      }
-    }
-    return false;
-  }
+      } else
+        for (const auto& to_vertex_edge_id : to_vertex_edges_ids)
+          if (from_vertex_edge_id == to_vertex_edge_id)
+            return true;
 
-  bool is_looped(const VertexId& vertex_id) const {
-    const auto& vertex_edges_ids = vertices_[vertex_id].get_edges_ids();
-    for (const auto& edge_id : vertex_edges_ids) {
-      const auto& connected_vertices = edges_[edge_id].connected_vertices;
-      if (connected_vertices[0] == connected_vertices[1])
-        return true;
-    }
     return false;
   }
 
   void connect_vertices(const VertexId& from_vertex_id,
                         const VertexId& to_vertex_id,
-                        const bool& paint) {
+                        const bool& initialization) {
     assert(is_vertex_exist(from_vertex_id));
     assert(is_vertex_exist(to_vertex_id));
+    assert(!is_connected(from_vertex_id, to_vertex_id));
 
-    if (from_vertex_id != to_vertex_id)
-      assert(!is_connected(from_vertex_id, to_vertex_id));
-    else
-      assert(!is_looped(from_vertex_id));
-
-    const auto& new_edge =
-        edges_.emplace_back(from_vertex_id, to_vertex_id, get_next_edge_id());
-
-    // add information into Vertex structure
-    vertices_[from_vertex_id].add_edge_id(new_edge.id);
-    if (from_vertex_id != to_vertex_id)
-      vertices_[to_vertex_id].add_edge_id(new_edge.id);
-
-    if (!paint) {
+    if (initialization) {
       int min_depth = vertices_[from_vertex_id].depth;
       for (const auto& edge_idx : vertices_[to_vertex_id].get_edges_ids()) {
         VertexId vert = edges_[edge_idx].connected_vertices[0];
         min_depth = min(min_depth, vertices_[vert].depth);
       }
       vertices_[to_vertex_id].depth = min_depth + 1;
-
-      if (depth_ < min_depth + 1)
-        depth_ = min_depth + 1;
+      depth_ = std::max(depth_, min_depth + 1);
     }
+    const int diff =
+        vertices_[to_vertex_id].depth - vertices_[from_vertex_id].depth;
 
-    if (paint) {
-      const int diff =
-          vertices_[to_vertex_id].depth - vertices_[from_vertex_id].depth;
-      if (from_vertex_id == to_vertex_id) {
-        edges_[new_edge.id].color = Color::GREEN;
-      } else if (diff == 0) {
-        edges_[new_edge.id].color = Color::BLUE;
-      } else if (diff == 1) {
-        edges_[new_edge.id].color = Color::YELLOW;
-      } else if (diff == 2) {
-        edges_[new_edge.id].color = Color::RED;
-      }
-    }
+    Color color;
+    if (initialization)
+      color = Color::Gray;
+    else if (from_vertex_id == to_vertex_id)
+      color = Color::Green;
+    else if (diff == 0)
+      color = Color::Blue;
+    else if (diff == 1)
+      color = Color::Yellow;
+    else if (diff == 2)
+      color = Color::Red;
+
+    const auto& new_edge = edges_.emplace_back(from_vertex_id, to_vertex_id,
+                                               get_next_edge_id(), color);
+    vertices_[from_vertex_id].add_edge_id(new_edge.id);
+    if (from_vertex_id != to_vertex_id)
+      vertices_[to_vertex_id].add_edge_id(new_edge.id);
   }
 
-  vector<Edge> get_edges() const { return edges_; }
-  vector<Vertex> get_vertices() const { return vertices_; }
+  const vector<Edge>& get_edges() { return edges_; }
+  const vector<Vertex>& get_vertices() { return vertices_; }
 
   int get_depth() const { return depth_; }
   int get_vertices_num() const { return vertices_.size(); }
@@ -238,53 +229,42 @@ class Graph {
   VertexId get_next_edge_id() { return edge_id_counter_++; }
 };
 
-void write_graph(const Graph& graph, const int& graph_number) {
+void write_graph(const Graph& graph) {
   std::ofstream out;
-  const std::string current_name =
-      JSON_GRAPH_FILENAME + "_" + to_string(graph_number) + ".gson";
-  out.open(current_name, std::ofstream::out | std::ofstream::trunc);
+  out.open(JSON_GRAPH_FILENAME, std::ofstream::out | std::ofstream::trunc);
   out << graph.to_json();
   out.close();
 }
 
 void new_vertices_generation(Graph& work_graph,
-                             const int& _depth,
+                             const int& depth,
                              const int& new_vertices_num) {
-  const int graph_depth = work_graph.get_depth();
-  int depth = min(graph_depth, _depth);
-
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<> dis(0, 1);
-
   for (int current_depth = 0; current_depth <= depth; current_depth++) {
     const double probability =
         static_cast<double>(current_depth) / static_cast<double>(depth);
-    for (const auto& vertex : work_graph.get_vertices()) {
-      if (vertex.depth == current_depth) {
+    for (const auto& vertex : work_graph.get_vertices())
+      if (vertex.depth == current_depth)
         for (int iter = 0; iter < new_vertices_num; iter++) {
           if (dis(gen) > probability) {
             work_graph.add_vertex();
             work_graph.connect_vertices(
                 vertex.id,
                 work_graph.get_vertices()[work_graph.get_vertices_num() - 1].id,
-                false);
+                true);
           }
         }
-      }
-    }
   }
 }
 
-void paint_edges(Graph& work_graph) {
+void paint_blue(Graph& work_graph) {
   const int graph_depth = work_graph.get_depth();
-
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<> dis(0, 1);
-  // BLUE
-
-  for (int current_depth = 1; current_depth < graph_depth; current_depth++) {
+  for (int current_depth = 1; current_depth <= graph_depth; current_depth++) {
     vector<Vertex> uni_depth_vertices;
     for (const auto& vertex : work_graph.get_vertices())
       if (vertex.depth == current_depth)
@@ -300,7 +280,7 @@ void paint_edges(Graph& work_graph) {
                                      adjacent_vertices[1]))
           if (dis(gen) < 0.25)
             work_graph.connect_vertices(adjacent_vertices[0],
-                                        adjacent_vertices[1], true);
+                                        adjacent_vertices[1], false);
       } else {
         adjacent_vertices[0] = adjacent_vertices[1];
         adjacent_vertices[1] = vertex.id;
@@ -308,56 +288,80 @@ void paint_edges(Graph& work_graph) {
                                      adjacent_vertices[1]))
           if (dis(gen) < 0.25)
             work_graph.connect_vertices(adjacent_vertices[0],
-                                        adjacent_vertices[1], true);
-      }
-    }
-  }
-
-  for (const auto& start_vertex : work_graph.get_vertices()) {
-    // GREEN
-    if (!work_graph.is_looped(start_vertex.id)) {
-      if (dis(gen) < 0.1) {
-        work_graph.connect_vertices(start_vertex.id, start_vertex.id, true);
-      }
-    }
-    // RED
-    if (dis(gen) < 0.33) {
-      if (start_vertex.depth + 2 <= graph_depth) {
-        vector<VertexId> red_vertices_ids;
-        for (const auto& end_vertex : work_graph.get_vertices()) {
-          if (end_vertex.depth == start_vertex.depth + 2)
-            if (!work_graph.is_connected(start_vertex.id, end_vertex.id))
-              red_vertices_ids.emplace_back(end_vertex.id);
-        }
-        if (red_vertices_ids.size() > 0) {
-          std::uniform_int_distribution<> distr(0, red_vertices_ids.size() - 1);
-          work_graph.connect_vertices(start_vertex.id,
-                                      red_vertices_ids[distr(gen)], true);
-        }
-      }
-    }
-    // YELLOW
-    const double probability = static_cast<double>(start_vertex.depth) /
-                               static_cast<double>(graph_depth);
-    if (dis(gen) < probability) {
-      vector<VertexId> yellow_vertices_ids;
-      for (const auto& end_vertex : work_graph.get_vertices()) {
-        if (end_vertex.depth == start_vertex.depth + 1) {
-          if (!work_graph.is_connected(start_vertex.id, end_vertex.id))
-            yellow_vertices_ids.push_back(end_vertex.id);
-        }
-      }
-      if (yellow_vertices_ids.size() > 0) {
-        std::uniform_int_distribution<> distr(0,
-                                              yellow_vertices_ids.size() - 1);
-        work_graph.connect_vertices(start_vertex.id,
-                                    yellow_vertices_ids[distr(gen)], true);
+                                        adjacent_vertices[1], false);
       }
     }
   }
 }
 
-void write_log(const Graph& graph,
+void paint_green(Graph& work_graph) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(0, 1);
+  for (const auto& start_vertex : work_graph.get_vertices())
+    if (!work_graph.is_connected(start_vertex.id, start_vertex.id))
+      if (dis(gen) < 0.1)
+        work_graph.connect_vertices(start_vertex.id, start_vertex.id, false);
+}
+
+void paint_red(Graph& work_graph) {
+  const int graph_depth = work_graph.get_depth();
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(0, 1);
+  for (const auto& start_vertex : work_graph.get_vertices()) {
+    if (dis(gen) < 0.33) {
+      if (start_vertex.depth + 2 <= graph_depth) {
+        vector<VertexId> Red_vertices_ids;
+        for (const auto& end_vertex : work_graph.get_vertices()) {
+          if (end_vertex.depth == start_vertex.depth + 2)
+            if (!work_graph.is_connected(start_vertex.id, end_vertex.id))
+              Red_vertices_ids.emplace_back(end_vertex.id);
+        }
+        if (Red_vertices_ids.size() > 0) {
+          std::uniform_int_distribution<> distr(0, Red_vertices_ids.size() - 1);
+          work_graph.connect_vertices(start_vertex.id,
+                                      Red_vertices_ids[distr(gen)], false);
+        }
+      }
+    }
+  }
+}
+
+void paint_yellow(Graph& work_graph) {
+  const int graph_depth = work_graph.get_depth();
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(0, 1);
+  for (const auto& start_vertex : work_graph.get_vertices()) {
+    const double probability = static_cast<double>(start_vertex.depth) /
+                               static_cast<double>(graph_depth);
+    if (dis(gen) < probability) {
+      vector<VertexId> Yellow_vertices_ids;
+      for (const auto& end_vertex : work_graph.get_vertices()) {
+        if (end_vertex.depth == start_vertex.depth + 1) {
+          if (!work_graph.is_connected(start_vertex.id, end_vertex.id))
+            Yellow_vertices_ids.push_back(end_vertex.id);
+        }
+      }
+      if (Yellow_vertices_ids.size() > 0) {
+        std::uniform_int_distribution<> distr(0,
+                                              Yellow_vertices_ids.size() - 1);
+        work_graph.connect_vertices(start_vertex.id,
+                                    Yellow_vertices_ids[distr(gen)], false);
+      }
+    }
+  }
+}
+
+void paint_edges(Graph& work_graph) {
+  paint_blue(work_graph);
+  paint_green(work_graph);
+  paint_red(work_graph);
+  paint_yellow(work_graph);
+}
+
+void write_log(Graph& work_graph,
                std::ofstream& logfile,
                const int& depth,
                const int& new_vertices_num,
@@ -367,12 +371,12 @@ void write_log(const Graph& graph,
   res += "time Graph " + to_string(graph_num) + ", Generation Ended {\n";
   res += "\tdepth: " + to_string(depth) + ",\n";
   res += "\tnew_vertices_num: " + to_string(new_vertices_num) + ",\n";
-  res += "vertices: " + to_string(graph.get_vertices_num()) + ", [";
+  res += "vertices: " + to_string(work_graph.get_vertices_num()) + ", [";
 
   std::vector<int> depth_count;
-  for (int iter = 0; iter < graph.get_depth(); iter++)
+  for (int iter = 0; iter < work_graph.get_depth(); iter++)
     depth_count.emplace_back(0);
-  for (const auto& vertex : graph.get_vertices()) {
+  for (const auto& vertex : work_graph.get_vertices()) {
     depth_count[vertex.depth]++;
   }
   for (const auto& depth : depth_count) {
@@ -381,29 +385,29 @@ void write_log(const Graph& graph,
   res.pop_back();
   res.pop_back();
   res += "],\n";
-  res += "edges: " + to_string(graph.get_edges_num()) + ", {";
+  res += "edges: " + to_string(work_graph.get_edges_num()) + ", {";
   std::array<int, COLORS_NUMBER> colors;
   for (int iter = 0; iter < COLORS_NUMBER; iter++)
     colors[iter] = 0;
-  for (const auto& edge : graph.get_edges()) {
+  for (const auto& edge : work_graph.get_edges()) {
     switch (edge.color) {
-      case Color::GRAY: {
+      case Color::Gray: {
         colors[0]++;
         break;
       }
-      case Color::GREEN: {
+      case Color::Green: {
         colors[1]++;
         break;
       }
-      case Color::BLUE: {
+      case Color::Blue: {
         colors[2]++;
         break;
       }
-      case Color::YELLOW: {
+      case Color::Yellow: {
         colors[3]++;
         break;
       }
-      case Color::RED: {
+      case Color::Red: {
         colors[4]++;
         break;
       }
@@ -425,31 +429,29 @@ int main() {
   std::ofstream log;
   log.open(LOG_FILENAME, std::ofstream::out | std::ofstream::trunc);
 
-  int depth = INVALID_ID;
+  int depth = INVALID_NEW_DEPTH;
   do {
     std::cout << "Enter generate graph depth" << endl;
     std::cin >> depth;
   } while (depth < 0);
-  int new_vertices_num = INVALID_NUMBER;
+  int new_vertices_num = INVALID_NEW_VERTICES_NUM;
   do {
     std::cout << "Enter new_vertices_num" << endl;
     std::cin >> new_vertices_num;
   } while (new_vertices_num < 0);
 
+  std::cout << "Depth of adding vertices: " << depth << endl;
+
   for (int graph_num = 0; graph_num < GRAPH_NUMBER; graph_num++) {
     Graph my_graph;
-    for (int iter = 0; iter < START_VERTICES_NUMBER; iter++)
-      my_graph.add_vertex();
-    for (int vertex_number = 0; vertex_number < START_VERTICES_NUMBER - 1;
-         vertex_number++)
-      my_graph.connect_vertices(vertex_number, vertex_number + 1, false);
-
+    my_graph.add_vertex();
     new_vertices_generation(my_graph, depth, new_vertices_num);
     paint_edges(my_graph);
-    write_graph(my_graph, graph_num);
+    write_graph(my_graph);
     write_log(my_graph, log, depth, new_vertices_num, graph_num);
   }
 
   log.close();
+
   return 0;
 }
