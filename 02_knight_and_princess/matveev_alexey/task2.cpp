119b51d7c002702f28d1d7dba3c8d86ab7873748
MatveevAlexy <matveev2001a@gmail.com>
2021-11-11 09:58:17 +0300

    task2

--- 02_knight_and_princess/matveev_alexey/task2.cpp
+++ 02_knight_and_princess/matveev_alexey/task2.cpp
@@ -12,29 +12,24 @@ struct Vertex {
   const VertexId id;
 
   explicit Vertex(const VertexId& _id) : id(_id) {}
-  void AddEdgeId(const EdgeId& _edge_id) {
+  /*void AddEdgeId(const EdgeId& _edge_id) {
     assert(!HasEdge(_edge_id) && "This vertex already has this edge");
     edge_ids_.push_back(_edge_id);
   }
   EdgeId IthEdgeId(int i) { return edge_ids_[i]; }
-  int ConnectedEdges() { return edge_ids_.size(); }
-  static int GenerateId() { return max_id_number_++; }
-  int EverGenerated() { return max_id_number_ + 1; }
+  int ConnectedEdges() { return edge_ids_.size(); }*/
 
  private:
-  std::vector<EdgeId> edge_ids_;
-  static int max_id_number_;
-  bool HasEdge(const EdgeId& _edge_id) {
+  //std::vector<EdgeId> edge_ids_;
+  /*bool HasEdge(const EdgeId& _edge_id) {
     for (const auto& edge_id : edge_ids_) {
       if (edge_id == _edge_id) {
         return true;
       }
     }
     return false;
-  }
+  }*/
 };
-
-int Vertex::max_id_number_ = 0;
 
 struct Edge {
   const EdgeId id;
@@ -58,7 +53,14 @@ class Graph {
   bool AreConnected(const VertexId& _vertex_id1, const VertexId& _vertex_id2) {
     assert(HasVertex(_vertex_id1) && "Vertex1 index is out of range");
     assert(HasVertex(_vertex_id2) && "Vertex2 index is out of range");
-    if (_vertex_id1 == _vertex_id2) {
+    for (const auto& edge : edges_) {
+        if ((edge.vertex_id1 == _vertex_id1 && edge.vertex_id2 == _vertex_id2) ||
+            (edge.vertex_id1 == _vertex_id2 && edge.vertex_id2 == _vertex_id1)) {
+            return true;
+        }
+    }
+    return false;
+    /*if (_vertex_id1 == _vertex_id2) {
       for (int i = 0; i < vertexes_[_vertex_id1].ConnectedEdges(); i++) {
         if (edges_[vertexes_[_vertex_id1].IthEdgeId(i)].vertex_id1 ==
                 _vertex_id2 &&
@@ -77,12 +79,11 @@ class Graph {
         }
       }
     }
-    return false;
+    return false;*/
   }
 
   void AddVertex() {
-    vertexes_.emplace_back(Vertex::GenerateId());
-    vertex_num_++;
+    vertexes_.emplace_back(vertex_new_id_++);
   }
 
   void AddEdge(const VertexId& _vertex_id1, const VertexId& _vertex_id2) {
@@ -90,19 +91,14 @@ class Graph {
     assert(HasVertex(_vertex_id2) && "Vertex2 index is out of range");
     assert(!AreConnected(_vertex_id1, _vertex_id2) &&
            "These vertexes are already connected");
-    edges_.emplace_back(edge_num_, std::min(_vertex_id1, _vertex_id2),
+    edges_.emplace_back(edge_new_id_++, std::min(_vertex_id1, _vertex_id2),
                         std::max(_vertex_id1, _vertex_id2));
-    vertexes_[_vertex_id1].AddEdgeId(edge_num_);
-    if (_vertex_id1 != _vertex_id2) {
-      vertexes_[_vertex_id2].AddEdgeId(edge_num_);
-    }
-    edge_num_++;
   }
 
  private:
   std::vector<Vertex> vertexes_;
   std::vector<Edge> edges_;
-  int vertex_num_ = 0, edge_num_ = 0;
+  int vertex_new_id_ = 0, edge_new_id_;
 };
 
 int main() {
