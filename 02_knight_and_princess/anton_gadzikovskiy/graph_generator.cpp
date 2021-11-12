350e57364b9540387f94059825139c1bc41fe15c
AGadzuk <gazikanton@mail.ru>
2021-11-12 12:56:40 +0300

    resolved fixes

--- 02_knight_and_princess/anton_gadzikovskiy/graph_generator.cpp
+++ 02_knight_and_princess/anton_gadzikovskiy/graph_generator.cpp
@@ -1,139 +1,97 @@
-﻿#include <fstream>
+#include <array>
+#include <fstream>
 #include <iostream>
-// using namespace std;
-const int num_of_vertices = 14;
-const int num_of_edges = 18;
-const int all_edges =
+#include <vector>
+using std::vector;
+constexpr int num_of_vertices = 14;
+constexpr int num_of_edges = 18;
+constexpr int all_edges =
     num_of_vertices * (num_of_vertices - 1) / 2 + num_of_vertices;
+vector<vector<int>> vertices = {
+    {0, 1, 2},       {0, 3, 4, 5}, {1, 6, 7}, {2, 8},      {3, 9},
+    {4, 10},         {5, 11},      {6, 12},   {7, 13},     {8, 14},
+    {9, 10, 11, 15}, {12, 13, 16}, {14, 17},  {15, 16, 17}};
+vector<vector<int>> edges = {{0, 1},   {0, 2},   {0, 3},  {1, 4},  {1, 5},
+                             {1, 6},   {2, 7},   {2, 8},  {3, 9},  {4, 10},
+                             {5, 10},  {6, 10},  {7, 11}, {8, 11}, {9, 12},
+                             {10, 13}, {11, 13}, {12, 13}};
 
-struct Vertex {
+struct VertexStruct {
   int id;
-  int edge_ids[all_edges];
+  vector<int> edgeIds;
 };
-struct Edge {
+struct EdgeStruct {
   int id;
-  int vertex_ids[2];
+  vector<int> vertexIds;
 };
 
-int ARRAY_SIZE_V(Vertex x[]) {
-  return (*(&x + 1) - x);
-}
-int ARRAY_SIZE_E(Edge x[]) {
-  return (*(&x + 1) - x);
-}
-
-void Vertex_output(Vertex v, std::ofstream& file) {
+void print_vertices(VertexStruct v, std::ofstream& file) {
   file << "{\n\t\t\t\"id\": " << v.id << ",\n\t\t\t\"edge_ids\": [";
-  for (int i = 0; i < std::size_t(v.edge_ids); i++) {
-    if (v.edge_ids[i] > -1) {
-      file << v.edge_ids[i] << ",";
+  for (auto i = v.edgeIds.begin(); i != v.edgeIds.end(); i++) {
+    int index = std::distance(v.edgeIds.begin(), i);
+    if (*i != v.edgeIds.back()) {
+      file << *i << ",";
     } else {
-      break;
+      file << *i;
     }
   }
-  file << "],\n\t\t}";
+  file << "]\n\t\t}";
 }
-void Edge_output(Edge e, std::ofstream& file) {
+void print_edges(EdgeStruct e, std::ofstream& file) {
   file << "{\n\t\t\t\"id\": " << e.id << ",\n\t\t\t\"vertex_ids\": [";
-  for (int i = 0; i < std::size_t(e.vertex_ids); i++) {
-    if (e.vertex_ids[i] > -1) {
-      file << e.vertex_ids[i] << ",";
+  for (auto i = e.vertexIds.begin(); i != e.vertexIds.end(); i++) {
+    int index = std::distance(e.vertexIds.begin(), i);
+    if (*i != e.vertexIds.back()) {
+      file << *i << ",";
     } else {
-      break;
+      file << *i;
     }
   }
-  file << "],\n\t\t}";
+  file << "]\n\t\t}";
 }
-void Graph_output(Vertex v[], Edge e[], std::ofstream& file) {
+void print_graph(VertexStruct v[], EdgeStruct e[], std::ofstream& file) {
   file << "{\n\t\"vertices\": [\n\t\t";
-  for (int i = 0; i < ARRAY_SIZE_V(v); i++) {
-    if (v[i].id > -1) {
-      Vertex_output(v[i], file);
-      if (i != (ARRAY_SIZE_V(v) - 1)) {
-        file << ",\n\t\t";
-      }
+  for (int i = 0; i < num_of_vertices; i++) {
+    print_vertices(v[i], file);
+    if (i != num_of_vertices - 1) {
+      file << ",\n\t\t";
     } else {
-      break;
+      file << "\n\t";
     }
   }
   file << "],\n\t\"edges\": [\n\t\t";
-  for (int i = 0; i < ARRAY_SIZE_E(e); i++) {
-    if (e[i].id > -1) {
-      Edge_output(e[i], file);
-      if (i != (ARRAY_SIZE_E(e) - 1)) {
-        file << ",\n\t\t";
-      }
+  for (int i = 0; i < num_of_edges; i++) {
+    print_edges(e[i], file);
+    if (i != num_of_edges - 1) {
+      file << ",\n\t\t";
     } else {
-      break;
+      file << "\n\t";
     }
   }
   file << "]\n}";
 }
 
 int main() {
-  // input
-  std::ifstream verin("vertices.txt");
-  std::ifstream edgin("edges.txt");
-  Vertex v[num_of_vertices];
-  Edge e[num_of_edges];
-  int index;
-  int current_v, current_e, incr = 0;
-  while (!verin.eof()) {
-    while (verin >> current_v) {
-      verin >> index;
-      v[current_v].id = current_v;
-      for (int i = 0; i < all_edges; i++) {
-        if (i < index) {
-          verin >> v[current_v].edge_ids[i];
-        } else {
-          v[current_v].edge_ids[i] = -1;
-        }
-      }
-      incr++;
-    }
-  }
-  std::cout << "vertices" << std::endl;
-  for (int i = 0; i < incr; i++) {
-    std::cout << i << ": ";
-    for (int j = 0; j < 4; j++) {
-      if (v[i].edge_ids[j] > -1) {
-        std::cout << (v[i].edge_ids[j]) << " ";
-      }
-    }
-    std::cout << "\n";
-  }
-  incr = 0;
-  while (!edgin.eof()) {
-    edgin >> current_e;
-    e[current_e].id = current_e;
-    edgin >> e[current_e].vertex_ids[0];
-    edgin >> e[current_e].vertex_ids[1];
-    incr++;
-  }
-  std::cout << "edges" << std::endl;
-  for (int i = 0; i < incr; i++) {
-    std::cout << i << ": ";
-    for (int j = 0; j < 2; j++) {
-      if (e[i].vertex_ids[j] > -1) {
-        std::cout << (e[i].vertex_ids[j]) << " ";
-      }
-    }
-    std::cout << "\n";
-  }
-  // generate simplest graph
-  // for (int i = 0; i < num_of_edges; i++)
-  //{
-  //    for (int j = 0; j < num_of_vertices; j++)
-  //    {
-  //
-  //    }
-  //}
+  VertexStruct verticesArray[num_of_vertices];
+  EdgeStruct edgesArray[num_of_edges];
 
-  // output as .json
+  for (auto it = vertices.begin(); it != vertices.end(); ++it) {
+    int index = std::distance(vertices.begin(), it);
+    VertexStruct newVertexStruct;
+    newVertexStruct.id = index;
+    newVertexStruct.edgeIds = *it;
+    verticesArray[index] = newVertexStruct;
+  }
 
-  std::ofstream gra;
-  gra.open("graph.json");
-  Graph_output(v, e, gra);
+  for (auto it = edges.begin(); it != edges.end(); ++it) {
+    int index = std::distance(edges.begin(), it);
+    EdgeStruct newEdgeStruct;
+    newEdgeStruct.id = index;
+    newEdgeStruct.vertexIds = *it;
+    edgesArray[index] = newEdgeStruct;
+  }
 
-  // graph.close();
+  std::ofstream graphOutput;
+  graphOutput.open("graph.json");
+  print_graph(verticesArray, edgesArray, graphOutput);
 }
