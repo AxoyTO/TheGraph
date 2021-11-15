67438262bab5f6034c3c6649c88d860f39e27d27
Procyon13 <veresk0013@ya.ru>
2021-11-15 17:34:58 +0300

    fix

--- 02_knight_and_princess/dana_stepina/main.cpp
+++ 02_knight_and_princess/dana_stepina/main.cpp
@@ -82,24 +82,25 @@ void Graph::add_edge(const VertexId& from_vertex_id,
   vertices_[to_vertex_id].add_edge_id(new_edge.id);
 }
 bool Graph::has_vertex_id(const VertexId& vertex_id) const {
-    for (const auto& vertex : vertices_)
-      if (vertex_id == vertex.id)
-        return true;
-    return false;
-  }
+  for (const auto& vertex : vertices_)
+    if (vertex_id == vertex.id)
+      return true;
+  return false;
+}
 bool Graph::is_connected(const VertexId& from_vertex_id,
-                    const VertexId& to_vertex_id) const {            
-    assert(has_vertex_id(from_vertex_id) &&
+                         const VertexId& to_vertex_id) const {
+  assert(has_vertex_id(from_vertex_id) &&
          "There is no such vertex in the graph.");
-    assert(has_vertex_id(to_vertex_id) &&
+  assert(has_vertex_id(to_vertex_id) &&
          "There is no such vertex in the graph.");
 
-    for (const auto& edge_id_from_vertex : vertices_[from_vertex_id].get_edge_ids())
-      for (const auto& edge_id_to_vertex : vertices_[to_vertex_id].get_edge_ids())
-          if (edge_id_from_vertex == edge_id_to_vertex) return true;
-    return false;
-  }
-
+  for (const auto& edge_id_from_vertex :
+       vertices_[from_vertex_id].get_edge_ids())
+    for (const auto& edge_id_to_vertex : vertices_[to_vertex_id].get_edge_ids())
+      if (edge_id_from_vertex == edge_id_to_vertex)
+        return true;
+  return false;
+}
 
 //ВЫВОД В ФАЙЛ JSON
 std::string get_vertex_string(const Vertex& vertex) {
