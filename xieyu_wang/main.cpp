8ece31bf9db52a54dfe8a063711d4eda1777234f
Xieyu Wang <varianovsky@outlook.com>
2021-11-19 02:37:08 +0800

    Too late Do on Friday......

--- xieyu_wang/main.cpp
+++ xieyu_wang/main.cpp
@@ -1,12 +1,29 @@
+/**
+ * Not done yet no need to check !!!
+ * done now:
+ *  change class Edge
+ *  change class Vertex
+ *  change class Graph
+ * todo:
+ * generator ..............todo
+ * main func...............todo
+ * comment: form 19/11/2021 >too late now to dont it.........
+ */
 #include <assert.h>
 #include <array>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
+constexpr float GREEN_EDGE_PROBABILITY = 0.1;
+constexpr float BLUE_EDGE_PROBABILITY = 0.25;
+constexpr float RED_EDGE_PROBABILITY = 0.33;
+
 class Vertex {
  public:
   const int id = 0;
-  explicit Vertex(int id) : id(id) {}
+  const int depth = 0;
+  explicit Vertex(int id, int depth) : id(id), depth(depth) {}
   void addEdgeId(int eId) {
     assert(!hasEdgeId(eId) && "Edge Id already exists");
     edgeIds_.push_back(eId);
@@ -19,7 +36,8 @@ class Vertex {
       str += std::to_string(edgeId) + ",";
     }
     str.pop_back();
-    str += "]}";
+    str += "], \"depth\": ";
+    str += std::to_string(depth) + "}";
     return str;
   }
   bool hasEdgeId(int id) const {
@@ -41,26 +59,33 @@ class Edge {
   const int id = 0;
   const int fromVertexId = 0;
   const int toVertexId = 0;
-  Edge(int id, int fromVertexId, int toVertexId)
-      : id(id), fromVertexId(fromVertexId), toVertexId(toVertexId) {}
+  const std::string color = "NULL";
+  Edge(int id, int fromVertexId, int toVertexId, std::string color)
+      : id(id),
+        fromVertexId(fromVertexId),
+        toVertexId(toVertexId),
+        color(color) {}
   std::string toString() const {
     std::string strEdge = "";
     strEdge += "\t\t{\"id\": " + std::to_string(id) + ",";
     strEdge += "\"vertex_ids\": [" + std::to_string(fromVertexId) + "," +
-               std::to_string(toVertexId) + "]}";
+               std::to_string(toVertexId) + "], color:\"" + color + "\" }";
     return strEdge;
   }
 };
 class Graph {
  public:
-  void addVertex() { vertices_.emplace_back(getNextVertexId()); }
-  void addEdge(int fromVertexId, int toVertexId) {
+  void addVertex(int depth) {
+    vertices_.emplace_back(getNextVertexId(), depth);
+  }
+  int getPresentVertexId() { return vertexIdCounter_; }
+  void addEdge(int fromVertexId, int toVertexId, std::string color) {
     assert(hasVertex(fromVertexId) && "Vertex doesn't exist");
     assert(hasVertex(toVertexId) && "Vertex doesn't exist");
     assert(!isConnected(fromVertexId, toVertexId) &&
            "Vertices already connected");
     const auto& newEdge =
-        edges_.emplace_back(getNextEdgeId(), fromVertexId, toVertexId);
+        edges_.emplace_back(getNextEdgeId(), fromVertexId, toVertexId, color);
     getVertex(fromVertexId).addEdgeId(newEdge.id);
     getVertex(toVertexId).addEdgeId(newEdge.id);
   }
@@ -122,7 +147,57 @@ class Graph {
   std::vector<Vertex> vertices_;
   std::vector<Edge> edges_;
 };
+class Generator {
+ public:
+  Generator(int maxDepth, int newVerticesNum)
+      : maxDeps_(maxDepth), newVerticesNum_(newVerticesNum) {}
+  // Recursion to create a table for default edge to connect them done
+  void verticesGenerator(int presentDepth = 0) {
+    for (int i = 0; i < newVerticesNum_; ++i) {
+      if (presentDepth < maxDeps_ &&
+          vertxGenerateProbabilityController(presentDepth)) {
+        int formVertex = graph.getPresentVertexId();
+        graph.addVertex(presentDepth);
+        int toVertexId = graph.getPresentVertexId();
+        std::vector<int> connection{formVertex, toVertexId, presentDepth};
+        connections_.push_back(connection);
+        verticesGenerator(presentDepth++);
+      }
+    }
+  }
+  // done with this function still need rgb to input
+  void grayEdgeGenerator() {
+    std::string color = "gray";
+    for (const auto& connection : connections_) {
+      graph.addEdge(connection.at(0), connection.at(1), color);
+    }
+  }
+  // done with this func
+  bool vertxGenerateProbabilityController(int depth) {
+    float probability = (maxDeps_ - depth) / maxDeps_ * 100;
+    srand((unsigned)time(NULL));          // default time clock for random
+    int randomNumber = rand() % 100 + 1;  // random from 1 to 100
+    if (randomNumber > probability) {
+      return false;
+    }
+    return true;
+  }
+
+ private:
+  Graph graph;
+  std::vector<std::vector<int>> connections_;
+  int maxDeps_;
+  int newVerticesNum_;
+};
 int main() {
+  int maxDeep = 0, newVerticesNum = 0;
+  std::cout << "Enter Max Depth:";
+  std::cin >> maxDeep;
+  std::cout << "Enter new vertices num:";
+  std::cin >> newVerticesNum;
+  if (newVerticesNum <= 0 || maxDeep <= 0) {
+    throw std::runtime_error("Wrong number");
+  }
   const int numberOfVertices = 18;
   const int verticesCount = 14;
   const std::array<std::pair<int, int>, numberOfVertices> connections = {
@@ -146,10 +221,10 @@ int main() {
        {12, 13}}};
   Graph graph;
   for (int i = 0; i < verticesCount; i++) {
-    graph.addVertex();
+    // graph.addVertex();
   }
   for (const auto& connection : connections) {
-    graph.addEdge(connection.first, connection.second);
+    // graph.addEdge(connection.first, connection.second);
   }
   std::ofstream writePT;
   writePT.open("Graph.json", std::ios::out);
