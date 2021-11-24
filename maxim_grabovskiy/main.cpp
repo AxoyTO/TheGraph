12eac77c26b0e708aaba3df6aa2e73c34035927a
maximgrab <90320622+maximgrab@users.noreply.github.com>
2021-11-24 19:33:45 +0300

    Add files via upload

--- maxim_grabovskiy/main.cpp
+++ maxim_grabovskiy/main.cpp
@@ -23,15 +23,15 @@ class Graph {
     vector<int> connectedEdges;
     explicit Vertex(VertexId _id) : id(_id) {}
   };
-  vector<Vertex> graphVertexes;
-  vector<Edge> graphEdges;
+  vector<Vertex> vertexes;
+  vector<Edge> edges;
   void addEdge(VertexId fromVertexId, VertexId toVertexId) {
     EdgeId tmp = getNewEdgeId();
-    graphEdges.emplace_back(tmp, fromVertexId, toVertexId);
-    graphVertexes[fromVertexId].connectedEdges.push_back(tmp);
-    graphVertexes[toVertexId].connectedEdges.push_back(tmp);
+    edges.emplace_back(tmp, fromVertexId, toVertexId);
+    vertexes[fromVertexId].connectedEdges.push_back(tmp);
+    vertexes[toVertexId].connectedEdges.push_back(tmp);
   }
-  void addVertex() { graphVertexes.emplace_back(getNewVertexId()); }
+  void addVertex() { vertexes.emplace_back(getNewVertexId()); }
   void spawnVertex(VertexId parentId) {
     addVertex();
     addEdge(parentId, vertexIdCounter_ - 1);
@@ -48,24 +48,23 @@ class GraphPrinter {
  public:
   GraphPrinter(Graph graph) {
     output << "{\n\"vertices\": [\n";
-    for (auto vertIter : graph.graphVertexes) {
+    for (auto vertIter : graph.vertexes) {
       output << "\t{ \"id\": " << vertIter.id << ", \"edge_ids\":[";
       for (int i = 0; i < (int)vertIter.connectedEdges.size() - 1; i++) {
         output << vertIter.connectedEdges[i] << ", ";
       }
       output << vertIter.connectedEdges[vertIter.connectedEdges.size() - 1]
              << "] }";
-      output << ((vertIter.id == (int)graph.graphVertexes.size() - 1)
-                     ? "\n ],\n"
-                     : ",\n");
+      output << ((vertIter.id == (int)graph.vertexes.size() - 1) ? "\n ],\n"
+                                                                 : ",\n");
     }
     output << " "
            << "\"edges\": [\n";
-    for (auto edgeIter : graph.graphEdges) {
+    for (auto edgeIter : graph.edges) {
       output << "\t{ \"id\": " << edgeIter.id << ", \"vertex_ids\":["
              << edgeIter.fromVertexId << ", " << edgeIter.toVertexId << "] }";
-      output << ((edgeIter.id == (int)graph.graphEdges.size() - 1) ? "\n ]\n}"
-                                                                   : ",\n");
+      output << ((edgeIter.id == (int)graph.edges.size() - 1) ? "\n ]\n}"
+                                                              : ",\n");
     }
   };
   string print() const {
@@ -81,27 +80,27 @@ void write_to_file(string output, string filename) {
   cout << output;
 }
 int main() {
-  Graph varGraph;
-  varGraph.addVertex();
-  varGraph.spawnVertex(0);
-  varGraph.spawnVertex(0);
-  varGraph.spawnVertex(0);
-  varGraph.spawnVertex(1);
-  varGraph.spawnVertex(1);
-  varGraph.spawnVertex(1);
-  varGraph.spawnVertex(2);
-  varGraph.spawnVertex(2);
-  varGraph.spawnVertex(3);
-  varGraph.spawnVertex(5);
-  varGraph.spawnVertex(7);
-  varGraph.spawnVertex(9);
-  varGraph.spawnVertex(10);
-  varGraph.addEdge(4, 10);
-  varGraph.addEdge(6, 10);
-  varGraph.addEdge(8, 11);
-  varGraph.addEdge(11, 13);
-  varGraph.addEdge(12, 13);
-  const auto graph_printer = GraphPrinter(varGraph);
+  Graph graph;
+  graph.addVertex();
+  graph.spawnVertex(0);
+  graph.spawnVertex(0);
+  graph.spawnVertex(0);
+  graph.spawnVertex(1);
+  graph.spawnVertex(1);
+  graph.spawnVertex(1);
+  graph.spawnVertex(2);
+  graph.spawnVertex(2);
+  graph.spawnVertex(3);
+  graph.spawnVertex(5);
+  graph.spawnVertex(7);
+  graph.spawnVertex(9);
+  graph.spawnVertex(10);
+  graph.addEdge(4, 10);
+  graph.addEdge(6, 10);
+  graph.addEdge(8, 11);
+  graph.addEdge(11, 13);
+  graph.addEdge(12, 13);
+  const auto graph_printer = GraphPrinter(graph);
   const auto graph_json = graph_printer.print();
   cout << graph_json;
   write_to_file(graph_json, "graph.json");
