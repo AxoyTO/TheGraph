a1547f35340a57a27516e5d100fb17dd112e6a34
lanzy4 <lanzy4@yandex.ru>
2021-12-02 20:22:06 +0300

    Add task_05

---
+++ kirill_tolstobrov/logger.cpp
@@ -0,0 +1,36 @@
+#include "logger.hpp"
+
+#include <fstream>
+#include <iostream>
+
+namespace uni_cpp_practice {
+void Logger::set_file(const std::optional<std::string>& filename) {
+  if (file_stream_.has_value()) {
+    file_stream_->close();
+    file_stream_ = std::nullopt;
+  }
+
+  if (!filename.has_value()) {
+    return;
+  }
+
+  file_stream_ = std::ofstream(filename.value());
+  if (!file_stream_->is_open()) {
+    throw std::runtime_error("Can't open log file");
+  }
+}
+
+void Logger::log(const std::string& string) {
+  if (file_stream_.has_value()) {
+    file_stream_.value() << string;
+  }
+  std::cout << string;
+}
+
+Logger::~Logger() {
+  if (file_stream_.has_value() && file_stream_->is_open()) {
+    file_stream_->close();
+    file_stream_ = std::nullopt;
+  }
+}
+}  // namespace uni_cpp_practice
