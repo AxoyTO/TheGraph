6001b7b7089d7624939c466aea3f2bdfa5130a2d
Procyon13 <veresk0013@ya.ru>
2021-12-09 10:09:22 +0300

    clang-format 1

--- dana_stepina/graph_generator.hpp
+++ dana_stepina/graph_generator.hpp
@@ -14,7 +14,6 @@ class GraphGenerator {
   };
 
   explicit GraphGenerator(const Params& params = Params()) : params_(params) {}
-  const Params& get_params() const { return params_; }
   Graph generate() const;
 
  private:
