73b7e8e0fa628da6ec5d7bb313f262a2bcd0f8ff
Flagro <antonpgrey@gmail.com>
2021-12-13 17:06:27 +0300

    formatting

--- anton_potapov/graph_traverser.cpp
+++ anton_potapov/graph_traverser.cpp
@@ -5,9 +5,8 @@
 #include "graph.hpp"
 #include "graph_traverser.hpp"
 
-std::map<VertexId, int> GraphTraverser::dynamic_bfs(
-    const Graph& graph,
-    int start_depth) {
+std::map<VertexId, int> GraphTraverser::dynamic_bfs(const Graph& graph,
+                                                    int start_depth) {
   // NOT WORKING: recursive call + returns depths only at last depths
   std::map<VertexId, int> depths;
   std::queue<VertexId> bfs_queue;
