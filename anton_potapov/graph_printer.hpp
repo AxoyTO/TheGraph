de139083da668600c086a0c0a2a7f4db2fdf398d
Flagro <antonpgrey@gmail.com>
2021-12-16 16:29:54 +0300

    made some private interfaces private

--- anton_potapov/graph_printer.hpp
+++ anton_potapov/graph_printer.hpp
@@ -9,10 +9,10 @@ class GraphPrinter {
   explicit GraphPrinter(Graph& input_graph);
   std::string print() const;
   std::string print_graph_description() const;
+  std::string print_vertex(const Vertex& vertex) const;
+  std::string print_edge(const Edge& Edge) const;
 
  private:
   Graph& graph_;
-  std::string print_vertex(const Vertex& vertex) const;
-  std::string print_edge(const Edge& Edge) const;
 };
 }  // namespace uni_cource_cpp
