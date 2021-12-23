3236bceab862f7abdc56ad4760eeff8cf82b59ad
Flagro <antonpgrey@gmail.com>
2021-12-23 14:03:17 +0300

    prepared main file and moved log messages generation logic

--- anton_potapov/main.cpp
+++ anton_potapov/main.cpp
@@ -1,7 +1,5 @@
-#include <chrono>
 #include <filesystem>
 #include <fstream>
-#include <sstream>
 #include <string>
 
 #include "config.hpp"
@@ -10,6 +8,7 @@
 #include "graph_generator.hpp"
 #include "graph_input_handler.hpp"
 #include "graph_printer.hpp"
+#include "log_messages_generator.hpp"
 #include "logger.hpp"
 
 using uni_cource_cpp::Graph;
@@ -18,45 +17,11 @@ using uni_cource_cpp::GraphGenerator;
 using uni_cource_cpp::GraphInputHandler;
 using uni_cource_cpp::GraphPrinter;
 using uni_cource_cpp::Logger;
+using uni_cource_cpp::LogMessagesGenerator;
 
 void prepare_temp_directory() {
   std::filesystem::create_directory(
       uni_cource_cpp::config::TEMP_DIRECTORY_PATH);
-}
-
-std::string get_current_date_time() {
-  const auto date_time = std::chrono::system_clock::now();
-  const auto date_time_t = std::chrono::system_clock::to_time_t(date_time);
-  std::stringstream date_time_string;
-  date_time_string << std::put_time(std::localtime(&date_time_t),
-                                    "%Y.%m.%d %H:%M:%S");
-  return date_time_string.str();
-}
-
-std::string generation_started_string(int id) {
-  std::stringstream generation_started_stringstream;
-  generation_started_stringstream << get_current_date_time() << ": Graph " << id
-                                  << ", Generation Started" << std::endl;
-  return generation_started_stringstream.str();
-}
-
-std::string generation_finished_string(int id, Graph& graph) {
-  std::stringstream generation_finished_stringstream;
-  generation_finished_stringstream
-      << get_current_date_time() << ": Graph " << id << ", Generation Finished "
-      << GraphPrinter(graph).print_graph_description() << std::endl;
-  return generation_finished_stringstream.str();
-}
-
-std::string generation_max_depth_warning(int id,
-                                         int max_generated_depth,
-                                         int given_depth) {
-  std::stringstream generation_max_depth_warning_stringstream;
-  generation_max_depth_warning_stringstream
-      << get_current_date_time() << ": Graph " << id
-      << "generated graph's depth=" << max_generated_depth
-      << " is less than specified one =" << given_depth << std::endl;
-  return generation_max_depth_warning_stringstream.str();
 }
 
 void write_to_file(const std::string& file_text, const std::string& file_path) {
@@ -67,6 +32,38 @@ void write_to_file(const std::string& file_text, const std::string& file_path) {
   }
   json_file << file_text << std::endl;
   json_file.close();
+}
+
+std::vector<Graph> generate_graphs(const GraphGenerator::Params& params,
+                                   int graphs_count,
+                                   int threads_count) {
+  auto generation_controller =
+      GraphGenerationController(threads_count, graphs_count, params);
+
+  auto& logger = Logger::get_logger();
+
+  auto graphs = std::vector<Graph>();
+  graphs.reserve(graphs_count);
+
+  generation_controller.generate(
+      [&logger](int index) {
+        logger.log(LogMessagesGenerator::generation_started_string(index));
+      },
+      [&logger, &graphs, &params](int index, Graph graph) {
+        const auto graph_printer = GraphPrinter(graph);
+        logger.log(LogMessagesGenerator::generation_finished_string(
+            index, graph_printer.print_graph_description()));
+        if (graph.depth() < params.depth) {
+          logger.log(LogMessagesGenerator::generation_max_depth_warning(
+              index, graph.depth(), params.depth));
+        }
+        graphs.push_back(graph);
+        write_to_file(graph_printer.print(),
+                      std::string(uni_cource_cpp::config::TEMP_DIRECTORY_PATH) +
+                          "graph_" + std::to_string(index) + ".json");
+      });
+
+  return graphs;
 }
 
 int main() {
@@ -82,20 +79,8 @@ int main() {
   const auto generator = GraphGenerator(params);
   auto& logger = Logger::get_logger();
 
-  for (int i = 0; i < graphs_count; i++) {
-    logger.log(generation_started_string(i));
-    auto graph = generator.generate_graph();
-    if (graph.depth() < depth) {
-      logger.log(generation_max_depth_warning(i, graph.depth(), depth));
-    }
-    logger.log(generation_finished_string(i, graph));
-
-    const auto graph_printer = GraphPrinter(graph);
-    const auto graph_json = graph_printer.print();
-    write_to_file(graph_json,
-                  std::string(uni_cource_cpp::config::TEMP_DIRECTORY_PATH) +
-                      "graph_" + std::to_string(i) + ".json");
-  }
+  const auto params = GraphGenerator::Params(depth, new_vertices_count);
+  const auto graphs = generate_graphs(params, graphs_count, threads_count);
 
   return 0;
 }
