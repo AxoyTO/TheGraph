6b76f5110afb0199b253c70b1a30ce8c4dc7757a
Robert <geleg.rigzin@mail.ru>
2021-12-23 11:59:55 +0300

    robert_gaisin/task_07

--- robert_gaisin/graph_generation_controller.cpp
+++ robert_gaisin/graph_generation_controller.cpp
@@ -4,13 +4,11 @@
 
 namespace uni_course_cpp {
 
-using std:thread;
-
 void GraphGenerationController::Worker::start() {
   assert(state_ != State::Working && "Worker is already working");
   state_ = State::Working;
 
-  thread_ = thread([&state_ = state_, &get_job_callback_ = get_job_callback_]() {
+  thread_ = std::thread([&state_ = state_, &get_job_callback_ = get_job_callback_]() {
         while (true) {
           if (state_ == State::ShouldTerminate) {
             state_ = State::Idle;
