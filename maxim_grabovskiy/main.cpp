61dff752ba8b7afaed0a6ea3809efdf6e2cd042f
maximgrab <90320622+maximgrab@users.noreply.github.com>
2021-12-24 19:07:58 +0300

    Add files via upload

--- maxim_grabovskiy/main.cpp
+++ maxim_grabovskiy/main.cpp
@@ -1,4 +1,4 @@
-#include <sys/stat.h>
+#include <filesystem>
 #include "config.hpp"
 #include "fstream"
 #include "graph.hpp"
@@ -9,7 +9,7 @@
 #include "logger.hpp"
 using std::string;
 void prepareTempDirectory() {
-  mkdir(config::kTempDirectoryPath, 0777);
+  std::filesystem::create_directory(uni_course_cpp::config::kTempDirectoryPath);
 }
 int intInput(string inputMessage) {
   int input = -1;
@@ -27,7 +27,8 @@ int intInput(string inputMessage) {
   return input;
 };
 void writeToFile(string const& output, string const& filename) {
-  std::ofstream fileToWrite(config::kTempDirectoryPath + filename);
+  std::ofstream fileToWrite(uni_course_cpp::config::kTempDirectoryPath +
+                            filename);
   fileToWrite << output;
 }
 string generationStartedString(int i) {
@@ -36,22 +37,23 @@ string generationStartedString(int i) {
 string generationFinishedString(int i, std::string string) {
   return ("Graph " + std::to_string(i) + ", generation finished" + string);
 }
-using namespace uni_course_cpp;
 int main() {
   int const depth = intInput("depth: ");
   int const newVerticesNum = intInput("new_vertex_num: ");
   const int graphsCount = intInput("graphs_count: ");
   prepareTempDirectory();
-  auto const params = GraphGenerator::Params(depth, newVerticesNum);
-  auto const generator = GraphGenerator(params);
-  auto& logger = Logger::getLogger();
+  auto const params =
+      uni_course_cpp::GraphGenerator::Params(depth, newVerticesNum);
+  auto const generator = uni_course_cpp::GraphGenerator(params);
+  auto& logger = uni_course_cpp::Logger::getLogger();
   for (int i = 0; i < graphsCount; i++) {
     logger.log(generationStartedString(i));
     auto const graph = generator.generate();
-    auto const graphPrinter = GraphPrinter(graph);
+    auto const graphPrinter = uni_course_cpp::GraphJsonPrinter(graph);
     auto const graphJson = graphPrinter.print();
     std::cout << graphJson << std::endl;
-    auto const graphDescription = printing::printGraph(graph);
+    auto const graphDescription =
+        uni_course_cpp::GraphPrinter::printGraph(graph);
     logger.log(generationFinishedString(i, graphDescription));
     writeToFile(graphJson, "graph_" + std::to_string(i) + ".json");
   }
